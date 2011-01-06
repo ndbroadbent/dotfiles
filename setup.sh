c282b7f6268d407c62f04c63699add52b922b4bf
Nathan Broadbent <nathan.f77@gmail.com>
2011-01-06 12:37:51 +0800

    Refactored setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+cp -f ndb_bashrc ~/.ndb_bashrc
 # Check if ndb_bashrc is already installed.
 if ! grep ". ~/.ndb_bashrc" ~/.bashrc; then
-    echo "== Installing custom bashrc extension.."
-    cp -f ndb_bashrc ~/.ndb_bashrc
     echo ". ~/.ndb_bashrc" >> ~/.bashrc
-    echo "===== Installed."
+    echo "== Installed custom bashrc."
 else
-    cp -f ndb_bashrc ~/.ndb_bashrc
-    echo "== Custom bashrc extension updated."
+    echo "== Updated custom bashrc."
 fi
+echo "== Please run this command to update your terminal : source ~/.bashrc"
+
