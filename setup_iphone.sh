eec3f75c8ea933ef62fbf5c9419bc6fb0bc1f5f7
Nathan Broadbent <nathan.f77@gmail.com>
2011-01-07 15:48:51 +0800

    Fixed some naming conventions

--- setup_iphone.sh
+++ setup_iphone.sh
@@ -4,6 +4,6 @@ iPhone="root@nathans-iphone"
 cat ./iphone_bashrc | \
 ssh $iPhone 'cat > /var/root/.bashrc && \
              if ! grep "source /var/root/.bashrc" /etc/profile; then \
-                  echo "source /var/root/.bashrc" >> /etc/profile;
+                  echo "source /var/root/.bashrc" >> /etc/profile; \
              fi'
 echo "== Installed iPhone bashrc on $iPhone."
