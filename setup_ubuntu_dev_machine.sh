56e09a4cde9dc9dbe0360a0332a8b9d8b25313f6
Nathan Broadbent <nathan.f77@gmail.com>
2011-01-07 16:09:51 +0800

    Added dev machine setup script

---
+++ setup_ubuntu_dev_machine.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# This bash script will run all the commands to
+# setup your development environment on Ubuntu (v=>9.10)
+
+echo "[ Starting installation script... ]"
+read -p "Please enter your name (for git account):"
+git_name="$REPLY"
+read -p "Please enter your email (for git account):"
+git_email="$REPLY"
+echo "== Please enter your sudo password =>"
+sudo echo "== Thanks. Now let me install some things for you..."
+
+echo "== Installing required software and development packages."
+sudo apt-get update
+sudo apt-get -ym install git-core gitk libyaml-ruby \
+libzlib-ruby libopenssl-ruby libxslt1-dev libxml2-dev \
+ack-grep vim gedit-plugins xclip mysql-server libmysql-ruby \
+libmysqlclient15-dev imagemagick libsqlite3-dev sqlite3 \
+sun-java6-jdk apache2
+
+echo "== Installing custom bashrc..."
+./setup_ubuntu_bashrc.sh
+source ~/.bashrc
+
+echo "== Installing rvm and ruby 1.9.2..."
+bash < <( curl http://rvm.beginrescueend.com/releases/rvm-install-head )
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"  # This loads RVM into a shell session.
+
+rvm install 1.9.2
+rvm use 1.9.2
+
+echo "== Setting up git and ssh.."
+git config --global user.name "$git_name"
+git config --global user.email "$git_email"
+ssh-keygen -t rsa -C "$git_email"
+
+echo "== Installing rails and other commonly used gems.."
+gem install rails cucumber cucumber-rails spree \
+gemcutter authlogic sqlite3-ruby mime-types rmagick \
+capistrano capistrano_colors mislav-will_paginate ruby-debug \
+heroku mechanize nokogiri
+
+echo "== Installing Python and required libraries for gedit plugins.."
+sudo apt-get -,y install python python-dev python-gtk2 python-gtk2-dev \
+python-webkit python-webkit-dev python-pyinotify
+
+echo "== Setting up gmate for gedit (RoR colors, etc).."
+./setup_ubuntu_gedit.sh
+
+echo "== Setting Capistrano to require colours.."
+cat >> ~/.caprc <<!
+require 'capistrano_colors'
+!
+
+echo "== Setting up autotest config.."
+cat >> ~/.autotest <<!
+Autotest.add_hook :initialize do |at|
+  %w{.svn .hg .git vendor}.each {|exception| at.add_exception(exception)}
+end
+!
+
+echo "== Installing Firefox extensions (Firebug and Web Developer Toolbar).."
+cd ~
+wget https://addons.mozilla.org/en-US/firefox/downloads/latest/60/addon-60-latest.xpi?src=addondetail
+wget https://addons.mozilla.org/en-US/firefox/downloads/latest/1843/addon-1843-latest.xpi?src=addondetail
+sudo firefox -install-global-extension *.xpi
+rm -f *.xpi
+
+echo "===== Ubuntu development machine has been set up."
---
+++ setup_ubuntu_gedit.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Sets up gedit for RoR development.
+SCRIPT_DIR=$(pwd)
+cd ~
+echo "==== Cloning gmate repository and installing package.."
+git clone git://github.com/lexrupy/gmate.git
+cd gmate
+echo "==== Setting up gedit plugins and configuration."
+    sudo cp mime/rails.xml /usr/share/mime/packages
+    sudo cp lang-specs/*.lang /usr/share/gtksourceview-2.0/language-specs/
+    sudo mkdir -p /usr/share/gedit-2/gmate
+    sudo cp gmate.py /usr/share/gedit-2/gmate/gmate.py
+    if [ ! -d /usr/share/gedit-2/plugins/taglist/ ]
+    then
+      sudo mkdir -p /usr/share/gedit-2/plugins/taglist/
+    fi
+    sudo cp tags/*.tags.gz /usr/share/gedit-2/plugins/taglist/
+    sudo update-mime-database /usr/share/mime
+    if [ ! -d $HOME/.gnome2/gedit ]
+    then
+      mkdir -p ~/.gnome2/gedit
+    fi
+    if [ ! -d $HOME/.gnome2/gedit/snippets ]
+    then
+      mkdir -p ~/.gnome2/gedit/snippets
+    fi
+    cp snippets/* ~/.gnome2/gedit/snippets/
+    if [ ! -d $HOME/.gnome2/gedit/plugins ]
+    then
+      mkdir -p ~/.gnome2/gedit/plugins
+    fi
+    cp -R plugins/* ~/.gnome2/gedit/plugins
+    if [ ! -d $HOME/.gnome2/gedit/styles ]
+    then
+      mkdir -p ~/.gnome2/gedit/styles
+    fi
+    cp styles/* ~/.gnome2/gedit/styles
+    cp $SCRIPT_DIR/geditcolors_nathan.xml ~/.gnome2/gedit/styles/nathan.xml
+
+    sudo sh ./debian/postinst
+    gconftool-2 --set /apps/gedit-2/plugins/active-plugins -t list --list-type=str [rails_extract_partial,rubyonrailsloader,align,smart_indent,text_tools,completion,quickhighlightmode,gemini,trailsave,rails_hotkeys,snapopen,filebrowser,snippets,modelines,smartspaces,docinfo,time,spell,terminal,drawspaces,codecomment,colorpicker,indent]
+    gconftool-2 --set /apps/gedit-2/preferences/editor/auto_indent/auto_indent -t bool true
+    gconftool-2 --set /apps/gedit-2/preferences/editor/bracket_matching/bracket_matching -t bool true
+    gconftool-2 --set /apps/gedit-2/preferences/editor/current_line/highlight_current_line -t bool true
+    gconftool-2 --set /apps/geditcd ~-2/preferences/editor/cursor_position/restore_cursor_position -t bool true
+    gconftool-2 --set /apps/gedit-2/preferences/editor/line_numbers/display_line_numbers -t bool true
+    gconftool-2 --set /apps/gedit-2/preferences/editor/right_margin/display_right_margin -t bool false
+    gconftool-2 --set /apps/gedit-2/preferences/editor/right_margin/right_margin_position -t int 80
+    gconftool-2 --set /apps/gedit-2/preferences/editor/colors/scheme -t str nathan
+    gconftool-2 --set /apps/gedit-2/preferences/editor/tabs/insert_spaces -t bool true
+    gconftool-2 --set /apps/gedit-2/preferences/editor/tabs/tabs_size -t int 4
+    gconftool-2 --set /apps/gedit-2/preferences/editor/wrap_mode/wrap_mode -t str GTK_WRAP_NONE
+    gconftool-2 --set /apps/gedit-2/preferences/editor/save/create_backup_copy -t bool false
+cd ..
+rm -rf gmate
