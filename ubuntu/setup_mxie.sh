5d85f20574ff834bd823b1bcb8e3f9c11d86c084
Nathan B <nathan.f77@gmail.com>
2011-01-11 11:36:45 +0800

    Added mxie setup script

---
+++ ubuntu/setup_mxie.sh
@@ -0,0 +1,13 @@
+# Requires 'alien', to install rpm packages on debian-based systems
+sudo apt-get install -ym alien
+cd /tmp
+rm mxie*.deb
+
+# Fetch mxie index page
+wget http://mx250/provisioning -O mxie_index.html
+# Parse out url of mxie rpm package.
+mxie_package=$(ruby -e 'puts File.open("mxie_index.html").read[/<a href="http:\/\/mx250:8000\/(mxie-.*\.rpm)/, 1]')
+
+wget http://mx250:8000/$mxie_package -O $mxie_package
+sudo alien -k $mxie_package
+sudo dpkg -i mxie*.deb
