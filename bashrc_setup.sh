49c7afe8f614d34a20e544a64095fe813b9c30e8
Nathan Broadbent <nathan.f77@gmail.com>
2011-04-14 23:57:09 +0800

    Added pull_bashrc alias to bashrc

--- bashrc_setup.sh
+++ bashrc_setup.sh
@@ -2,6 +2,7 @@
 cp -f custom_bashrc ~/.custom_bashrc
 # Check if ubuntu_bashrc is already installed.
 if ! (grep ". ~/.custom_bashrc" ~/.bashrc); then
+    echo "alias pull_bashrc='cd $(pwd) && git pull origin master && ./bashrc_setup.sh'" >> ~/.bashrc
     echo ". ~/.custom_bashrc" >> ~/.bashrc
     echo "== Installed custom bashrc."
 else
@@ -10,14 +11,4 @@ fi
 source ~/.bashrc
 
 echo "== If you did not run: [ . bashrc_setup.sh ], then run this command: [ source ~/.bashrc ]"
-
-echo "
-[Desktop Entry]
-Name=bashrc Github Sync
-GenericName=bashrc Github Sync
-Comment=Sync bashrc with github repo
-Exec=cd $(pwd) && git pull origin master && ./bashrc_setup.sh
-Type=Application
-Categories=Network;
-StartupNotify=false" > ~/.config/autostart/update_bashrc.desktop
 
