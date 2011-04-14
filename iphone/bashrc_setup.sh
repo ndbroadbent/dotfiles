ba74df48efaeda554f55342afbc6b3c5b9745bde
Nathan Broadbent <nathan.f77@gmail.com>
2011-04-14 23:25:41 +0800

    Renamed bashrc setup script for iphone

--- iphone/bashrc_setup.sh
+++ iphone/bashrc_setup.sh
@@ -3,7 +3,8 @@ iPhone="root@nathans-iphone"
 
 cat ./iphone_bashrc | \
 ssh $iPhone 'cat > /var/root/.bashrc && \
-             if ! grep "source /var/root/.bashrc" /etc/profile; then \
+             if ! (grep "source /var/root/.bashrc" /etc/profile); then \
                   echo "source /var/root/.bashrc" >> /etc/profile; \
              fi'
 echo "== Installed iPhone bashrc on $iPhone."
+
