e9390ebf12f4b124ca6912d6fcde3271a7624ed8
Nathan Broadbent <nathan.f77@gmail.com>
2011-04-15 08:40:35 +1200

    Updated pull_bashrc command to live in custom bash rc and have the filepath sed'd in

--- bashrc_setup.sh
+++ bashrc_setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cp -f custom_bashrc ~/.custom_bashrc
+# Update the 'update' command with the current directory. (# delimiter)
+this_dir=$(pwd); sed -i s#--MYNIX_DIR--#$this_dir#g ~/.custom_bashrc
+
 # Check if ubuntu_bashrc is already installed.
 if ! (grep ". ~/.custom_bashrc" ~/.bashrc); then
-    echo "alias pull_bashrc='cd $(pwd) && git pull origin master && ./bashrc_setup.sh'" >> ~/.bashrc
     echo ". ~/.custom_bashrc" >> ~/.bashrc
     echo "== Installed custom bashrc."
 else
