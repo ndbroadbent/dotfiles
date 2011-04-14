2896d060060cd5a1f37fbb22600895f2bfd4965e
Nathan Broadbent <nathan.f77@gmail.com>
2011-04-14 23:42:56 +0800

    Added auto-sync for bashrc on system startup

--- bashrc_setup.sh
+++ bashrc_setup.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 cp -f custom_bashrc ~/.custom_bashrc
 # Check if ubuntu_bashrc is already installed.
-if ! grep ". ~/.custom_bashrc" ~/.bashrc; then
+if ! (grep ". ~/.custom_bashrc" ~/.bashrc); then
     echo ". ~/.custom_bashrc" >> ~/.bashrc
     echo "== Installed custom bashrc."
 else
     echo "== Updated custom bashrc."
 fi
-echo "== Please run this command to update your terminal : source ~/.bashrc"
+source ~/.bashrc
+
+echo "== If you did not run: [ . bashrc_setup.sh ], then run this command: [ source ~/.bashrc ]"
+
+echo "
+[Desktop Entry]
+Name=bashrc Github Sync
+GenericName=bashrc Github Sync
+Comment=Sync bashrc with github repo
+Exec=cd $(pwd) && git pull origin master && ./bashrc_setup.sh
+Type=Application
+Categories=Network;
+StartupNotify=false" > ~/.config/autostart/update_bashrc.desktop
 
