5fbdb92058216a3381ab0170e90fcb1cd54a58cb
Nathan Broadbent <nathan.f77@gmail.com>
2011-04-27 13:03:36 +0800

    Updates. Fixed prompt_command to work properly

--- iphone_bashrc_setup.sh
+++ iphone_bashrc_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 iPhone="root@nathans-iphone"
 
-cat assets/iphone_bashrc | \
+cat assets/iphone_bashrc.sh | \
 ssh $iPhone 'cat > /var/root/.bashrc && \
              if ! (grep "source /var/root/.bashrc" /etc/profile); then \
                   echo "source /var/root/.bashrc" >> /etc/profile; \
