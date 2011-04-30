a83000f444185793f681fd7037c12f84937b0067
Nathan Broadbent <nathan.f77@gmail.com>
2011-04-30 12:52:38 +0800

    Added gtk config for fonts, etc

--- gtk_setup.sh
+++ gtk_setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 this_dir=$(pwd)
+echo "Copying themes..."
 cp -rf assets/gtk_themes/* ~/.themes
+echo "Configuring gtk fonts, etc..."
+gconftool-2 --load assets/gtk_conf.xml
 
 if [ -z `which gtk-theme-switch2` ]; then
   echo "== Install gtk-theme-switch with this command:"
