c5cf71d8a46b292f410ee03416bc179ead4e9967
Nathan Broadbent <nathan.f77@gmail.com>
2011-04-30 13:09:10 +0800

    Added 'update' script.

--- dev_machine_install.sh
+++ dev_machine_install.sh
@@ -17,23 +17,7 @@ libzlib-ruby libopenssl-ruby libxslt1-dev libxml2-dev \
 ack-grep vim gedit-plugins xclip mysql-server libmysql-ruby \
 libmysqlclient15-dev imagemagick libsqlite3-dev sqlite3 \
 sun-java6-jdk apache2 python python-dev python-gtk2 python-gtk2-dev \
-python-webkit python-webkit-dev python-pyinotify \
-trash-cli
-
-echo "== Installing custom bashrc..."
-./bashrc_setup.sh
-
-
-echo "== Setting up .gemrc to use --no-ri and --no-rdoc..."
-echo "gem: --no-ri --no-rdoc" >> ~/.gemrc
-
-echo "== Installing rvm and ruby 1.9.2..."
-bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"  # This loads RVM into a shell session.
-
-rvm install 1.9.2
-rvm use 1.9.2
+python-webkit python-webkit-dev python-pyinotify gtk-theme-switch
 
 echo "== Setting up git and ssh.."
 git config --global user.name "$git_name"
@@ -42,33 +26,43 @@ git config --global branch.master.remote origin
 git config --global branch.master.merge refs/heads/master
 ssh-keygen -t rsa -C "$git_email"
 
+echo "== Installing custom bashrc..."
+./bashrc_setup.sh
+
+if [ -z `which rvm` ]; then
+  echo "== Installing rvm and ruby 1.9.2..."
+  bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
+  [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"  # Loads RVM into shell
+  rvm install 1.9.2
+  rvm use 1.9.2
+else
+  echo == "RVM already installed."
+fi
+
+# Set .gemrc to use --no-ri and --no-rdoc
+echo "gem: --no-ri --no-rdoc" > ~/.gemrc
+
 echo "== Installing rails and other commonly used gems.."
 gem install rails cucumber cucumber-rails spree \
 gemcutter authlogic sqlite3-ruby mime-types rmagick \
 capistrano capistrano_colors mislav-will_paginate ruby-debug \
 heroku mechanize nokogiri
 
-echo "== Setting up gmate for gedit (RoR colors, etc).."
-./gedit_setup.sh
-
-echo "== Setting Capistrano to require colours.."
-cat >> ~/.caprc <<!
-require 'capistrano_colors'
-!
-
-echo "== Setting up autotest config.."
-cat >> ~/.autotest <<!
+# Require colors for capistrano
+echo "require 'capistrano_colors'" > ~/.caprc
+# Autotest config
+cat > ~/.autotest <<!
 Autotest.add_hook :initialize do |at|
   %w{.svn .hg .git vendor}.each {|exception| at.add_exception(exception)}
 end
 !
 
-# -- -install-global-extensions has been removed from firefox.
-#echo "== Installing Firefox extensions (Firebug and Web Developer Toolbar).."
-#cd ~
-#wget https://addons.mozilla.org/en-US/firefox/downloads/latest/60/addon-60-latest.xpi?src=addondetail
-#wget https://addons.mozilla.org/en-US/firefox/downloads/latest/1843/addon-1843-latest.xpi?src=addondetail
-# ~/.mozilla/firefox/<profile folder>/extensions
+echo "== Setting up gedit customizations (RoR colors, etc).."
+./gedit_setup.sh
+
+echo "== Setting up gtk fonts and themes.."
+./gtk_setup.sh
+
 
 echo "===== Ubuntu development machine has been set up."
 
