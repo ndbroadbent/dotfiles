451d4cfa3f30703a790a75c10f2840c11a65896f
Nathan Broadbent <nathan.f77@gmail.com>
2011-05-02 04:51:34 +0800

    fix for overwriting conkycolors dir

--- conky_setup.sh
+++ conky_setup.sh
@@ -3,6 +3,7 @@
 sudo apt-get install -ym conky-all
 
 # Copy conky files.
+rm -rf ~/.conkycolors
 cp -rf assets/conkycolors ~/.conkycolors
 
 echo -e "== Add the following command to your startup applications:\n    conky -c ~/.conkycolors/conkyrc"
