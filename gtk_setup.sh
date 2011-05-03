da57785bc6c92b214377166dc9013cd3f42424e3
Nathan Broadbent <nathan.f77@gmail.com>
2011-05-03 23:16:33 +0800

    Added compiz profile and faenza icon set installation.

--- gtk_setup.sh
+++ gtk_setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 echo "Copying themes..."
 cp -rf assets/gtk_themes/* ~/.themes
+echo "Installing icons..."
+if ! (apt-cache search faenza-icon-theme | grep -q faenza); then
+  sudo add-apt-repository ppa:tiheum/equinox
+  sudo apt-get update
+fi
+sudo apt-get install faenza-icon-theme
+
 echo "Configuring gtk fonts, etc..."
 gconftool-2 --load assets/gtk_conf.xml
 
