3d4aad2691a4848579dcb7cefc5a035bb717bff7
Nathan Broadbent <nathan.f77@gmail.com>
2011-05-04 17:41:53 +0800

    Need psql dev files for ruby

--- packages_setup.sh
+++ packages_setup.sh
@@ -12,7 +12,7 @@ sudo apt-get -ym install git-core curl ack-grep vim \
 gedit-plugins xclip synergy gitk \
 libyaml-ruby libzlib-ruby libopenssl-ruby libmysql-ruby \
 imagemagick libxslt1-dev libxml2-dev \
-mysql-server postgresql sqlite3 libmysqlclient15-dev libsqlite3-dev \
+mysql-server postgresql sqlite3 libmysqlclient15-dev libpq-dev libsqlite3-dev \
 apache2 python python-webkit python-webkit-dev python-pyinotify \
 compiz ttf-droid compizconfig-settings-manager cortina nautilus-dropbox
 
