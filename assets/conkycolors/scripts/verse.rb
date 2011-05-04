3f0a3eff78ff7f46c2fa463e0a06cbb3a21f7374
Nathan B <nathan.f77@gmail.com>
2011-05-04 10:49:10 +0800

    Added votd to conky

---
+++ assets/conkycolors/scripts/verse.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# Formats output of 'verse' nicely.
+verse,ref=`verse`.split(/ {11}/)
+verse=verse.gsub(/  +/," ").gsub("\n","").scan(/.{1,29}/).
+  join("\n<br>").gsub(/([^ ]*)\n<br>/m,"\n"<<"\\1").strip
+ref=ref.strip
+case ARGV.first
+when "ref"
+  puts ref
+when "verse"
+  puts verse
+else
+  puts "#{verse}\n#{ref}"
+end
+
