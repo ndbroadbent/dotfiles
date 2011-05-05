e839654ec260695ca696ed2fd53eca08f1e88be2
Nathan Broadbent <nathan.f77@gmail.com>
2011-05-06 02:39:39 +0800

    Fixed conky

--- assets/conkycolors/scripts/verse.rb
+++ assets/conkycolors/scripts/verse.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
 # Formats output of 'verse' nicely.
 verse = `verse`
-verse, ref = verse.gsub(ref, "").strip, verse[Regexp.new('"  +(.*)$', Regexp::MULTILINE),1].to_s.strip
-verse=verse.gsub(/  +/," ").gsub("\n","").scan(/.{1,29}/).
+ref = verse[Regexp.new('"  +(.*)$', Regexp::MULTILINE),1].to_s.strip
+verse = verse.gsub(ref, "").strip
+verse=verse.gsub(/  +/," ").gsub("\n","").scan(/.{1,33}/).
   join("\n<br>").gsub(/([^ ]*)\n<br>/m,"\n"<<"\\1").strip
 ref=ref.strip
 case ARGV.first
