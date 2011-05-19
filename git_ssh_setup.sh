9c4c34b8e89010fc9ee39441d536575409759e67
Nathan Broadbent <nathan.f77@gmail.com>
2011-05-19 12:48:46 +0800

    Added vim as default git commit message editor

--- git_ssh_setup.sh
+++ git_ssh_setup.sh
@@ -7,6 +7,7 @@ if [ -n "$2" ]; then
   git config --global user.email "$git_email"
   git config --global branch.master.remote origin
   git config --global branch.master.merge refs/heads/master
+  git config --global core.editor vim  
   ssh-keygen -t rsa -C "$git_email"
   echo "===== Done."
 else
