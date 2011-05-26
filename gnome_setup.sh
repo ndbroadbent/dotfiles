dfdf5d997ea9385953e4aa58d71ce8e2a9801547
Nathan Broadbent <nathan.f77@gmail.com>
2011-05-26 11:22:51 +0800

    Added inconsolata font as system monospace font.

--- gnome_setup.sh
+++ gnome_setup.sh
@@ -2,12 +2,18 @@
 echo "== Copying themes..."
 mkdir -p ~/.themes
 cp -rf assets/gtk_themes/* ~/.themes
-echo "== Installing icons..."
-if ! (apt-cache search faenza-icon-theme | grep -q faenza); then
-  sudo add-apt-repository ppa:tiheum/equinox
-  sudo apt-get update
-fi
-sudo apt-get install -ym faenza-icon-theme
+echo "== Installing icons, fonts, compiz, etc..."
+# PPAs
+# --------------------------------------------------------------
+# Faenza icons
+sudo add-apt-repository ppa:tiheum/equinox
+# Cortina (wallpaper switcher)
+sudo add-apt-repository ppa:cs-sniffer/cortina
+sudo apt-get update
+
+sudo apt-get install -ym faenza-icon-theme \
+compiz compizconfig-settings-manager cortina \
+ttf-droid ttf-inconsolata
 
 echo "== Configuring gnome font preferences..."
 gconftool-2 --load assets/gnome_fonts_conf.xml
