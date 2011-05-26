a9e4cd22052575272f4bfd481d07bc695e7c1821
Nathan Broadbent <nathan.f77@gmail.com>
2011-05-26 11:22:51 +0800

    Added inconsolata font as system monospace font.

--- packages_setup.sh
+++ packages_setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "== Installing required software and development packages."
-# Wallpaper switcher ppa
-sudo add-apt-repository ppa:cs-sniffer/cortina
 # Dropbox source
 echo "deb http://linux.dropbox.com/ubuntu $(lsb_release -cs) main" | sudo tee "/etc/apt/sources.list.d/dropbox.list" > /dev/null
 sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
@@ -14,8 +12,7 @@ libyaml-ruby libzlib-ruby libopenssl-ruby libmysql-ruby \
 imagemagick libmagickwand-dev libmagickcore-dev libxslt1-dev libxml2-dev \
 mysql-server postgresql sqlite3 libmysqlclient15-dev libpq-dev libsqlite3-dev \
 apache2 python python-webkit python-webkit-dev python-pyinotify \
-compiz ttf-droid compizconfig-settings-manager cortina nautilus-dropbox \
-thunderbird libnotify-bin \
+nautilus-dropbox thunderbird libnotify-bin \
 avant-window-navigator avant-window-navigator-data awn-settings \
 awn-applets-c-core libawn1 vala-awn
 
