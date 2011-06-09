3326ebfe919f49b55fbbd22553400c5ab8ed5bfb
Nathan Broadbent <nathan.f77@gmail.com>
2011-06-10 00:51:45 +0800

    Added --update-all command to update all git repos

--- assets/custom_bashrc.sh
+++ assets/custom_bashrc.sh
@@ -211,55 +211,43 @@ function prj() {
   if [ -n "$1" ]; then
     if [ "$1" = "--rebuild-cache" ]; then
       _prj_rebuild_cache
+    elif [ "$1" = "--update-all" ]; then
+      _prj_git_update_all
     else
       _prj_check_cache
       path=`grep -m1 "$1" $src_dir/.git_index`
       if [ -n "$path" ]; then
         # Change to project directory. This will automatically execute the .rvmrc
         cd $src_dir/$path
-        # If there are any local changes, print them.
-        if ! [ `git status --porcelain | wc -l` -eq 0 ]; then
-          gst
-        else
-          # Check that a local 'origin' remote exists.
-          if (git remote -v | grep -q origin); then
-            branch=`parse_git_branch`
-            # If we aren't on any branch, checkout master.
-            if [ "$branch" = "(no branch)" ]; then
-              echo -e "=== Checking out$_git_col master$_txt_col branch."
-              git checkout master
-              branch="master"
-            fi
-            echo -e "=== Updating code in $_bld_col$path$_txt_col from$_git_col origin/$branch$_txt_col... (Press Ctrl+C to cancel)"
-            # Pull the latest code from the server for the checked out branch.
-            git pull origin $branch
-          fi
-        fi
+        # Run git commands (either update or show changes)
+        _prj_git_pull_or_status
       else
         echo -e "$_wrn_col'$1' did not match any git repos in $src_dir$_txt_col"
       fi
     fi
   else
-    cat $src_dir/.git_index | sed -e "s/.*\///" -e "s/--.*/" | sort
+    echo -e "Git repositories in $_bld_col$src_dir$_txt_col:\n"
+    cat $src_dir/.git_index | sed -e "s/--.*//" | grep . | sort
   fi
 }
 
-# Rescursively lists git repos in $src_dir
-function list_git_projects() {
-  for repo in `find $src_dir -mindepth 2 -type d -name .git`; do
+# Rescursively searches for git repos in $src_dir
+function _prj_find_git_repos() {
+  for repo in $(find $src_dir -mindepth 2 -type d -name .git); do
     expr match "$repo" "$src_dir\/\(.*\)/.git"
   done
 }
 
 # Rebuilds cache of git repos in $src_dir.
 function _prj_rebuild_cache() {
-  local repo_count
   echo -e "== Scanning $src_dir for git repos..."
-  list_git_projects > $src_dir/.git_index
-  repo_count=`cat $src_dir/.git_index | wc -l`
-  # Extra commands
-  echo "--rebuild-cache" >> $src_dir/.git_index
-  echo -e "===== Cached $_bld_col$repo_count$_txt_col repos in $src_dir/.git_index"
+  _prj_find_git_repos > $src_dir/.git_index
+  # Append extra commands
+  cat >>$src_dir/.git_index <<EOF
+--rebuild-cache
+--update-all
+EOF
+  echo -e "===== Cached $_bld_col$(_prj_repo_count)$_txt_col repos in $src_dir/.git_index"
 }
 
 # Build cache if cache file doesn't exist.
@@ -267,6 +255,42 @@ function _prj_check_cache() {
   if [ ! -f $src_dir/.git_index ]; then
     _prj_rebuild_cache
   fi
+}
+
+# Produces a count of repos in the tab completion cache (excluding commands)
+function _prj_repo_count() {
+  echo $(cat $src_dir/.git_index | sed -e "s/--.*//" | grep . | wc -l)
+}
+
+
+# If the working directory is clean, update the git repository. Otherwise, show changes
+function _prj_git_pull_or_status() {
+  if ! [ `git status --porcelain | wc -l` -eq 0 ]; then
+    gst
+  else
+    # Check that a local 'origin' remote exists.
+    if (git remote -v | grep -q origin); then
+      branch=`parse_git_branch`
+      # If we aren't on any branch, checkout master.
+      if [ "$branch" = "(no branch)" ]; then
+        echo -e "=== Checking out$_git_col master$_txt_col branch."
+        git checkout master
+        branch="master"
+      fi
+      echo -e "=== Updating code in $_bld_col$path$_txt_col from$_git_col origin/$branch$_txt_col... (Press Ctrl+C to cancel)"
+      # Pull the latest code from the server for the checked out branch.
+      git pull origin $branch
+    fi
+  fi
+}
+
+# Updates all git repositories where possible
+function _prj_git_update_all() {
+  echo -e "== Updating code in $_bld_col$(_prj_repo_count)$_txt_col repos...\n"
+  for path in $(cat $src_dir/.git_index | sed -e "s/--.*//" | grep . | sort); do
+    cd $src_dir/$path
+    _prj_git_pull_or_status
+  done
 }
 
 # Tab completion function for prj()
