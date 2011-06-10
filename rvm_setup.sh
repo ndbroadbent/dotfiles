846c998cedcf41b33b6ea205a2517698f4cfa7bc
Nathan Broadbent <nathan.f77@gmail.com>
2011-06-10 16:47:41 +0800

    Added rvm hook generation to setup script

--- rvm_setup.sh
+++ rvm_setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+src_dir=`echo ~/src`
+
 if [ -z `which rvm` ]; then
   echo "== Installing rvm and ruby 1.9.2..."
   bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
@@ -9,4 +11,8 @@ else
   echo "== Updating rvm..."
   rvm update
 fi
+
+# RVM after_use hook: updates a symlink to point to the current gemset
+# ---------------------------------------------------------------------
+echo "ln -nfs \$rvm_ruby_gem_home/gems $src_dir/current_gemset" > ~/.rvm/hooks/after_use
 
