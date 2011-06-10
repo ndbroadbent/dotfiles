53551fdb03c2204a56cfdde9c404b3c55d6793c1
Nathan Broadbent <nathan.f77@gmail.com>
2011-06-10 15:42:22 +0800

    Comment

--- assets/custom_bashrc.sh
+++ assets/custom_bashrc.sh
@@ -263,7 +263,7 @@ function _prj_repo_count() {
 }
 
 
-# If the working directory is clean, update the git repository. Otherwise, show changes
+# If the working directory is clean, update the git repository. Otherwise, show changes.
 function _prj_git_pull_or_status() {
   if ! [ `git status --porcelain | wc -l` -eq 0 ]; then
     gst
