962da5d55c02f5841780a0df5beee38020a00db8
Nathan Broadbent <nathan.f77@gmail.com>
2011-06-10 10:26:29 +0800

    How is VLC not the default media player yet?

--- packages_setup.sh
+++ packages_setup.sh
@@ -12,7 +12,7 @@ libyaml-ruby libzlib-ruby libopenssl-ruby libmysql-ruby \
 imagemagick libmagickwand-dev libmagickcore-dev libxslt1-dev libxml2-dev \
 mysql-server postgresql sqlite3 libmysqlclient15-dev libpq-dev libsqlite3-dev \
 apache2 python python-webkit python-webkit-dev python-pyinotify \
-nautilus-dropbox nautilus-image-converter thunderbird libnotify-bin \
+nautilus-dropbox nautilus-image-converter vlc thunderbird libnotify-bin \
 avant-window-navigator avant-window-navigator-data awn-settings \
 awn-applets-c-core libawn1 vala-awn
 
