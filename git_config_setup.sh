b61b2cd92ac87c6509c32abc08a305b0d9d563d1
Nathan Broadbent <nathan.f77@gmail.com>
2011-06-11 12:34:17 +0800

    Simplified git config by just saving the raw file.

--- git_config_setup.sh
+++ git_config_setup.sh
@@ -5,14 +5,16 @@ if [ -n "$2" ]; then
 fi
 if [ -n "$git_name" ]; then
   echo "== Setting up git config..."
-  git config --global user.name "$git_name"
-  git config --global user.email "$git_email"
-  git config --global branch.master.remote origin
-  git config --global branch.master.merge refs/heads/master
-  git config --global core.editor vim
+  cat > ~/.gitconfig <<EOF
+[user]
+	name = $git_name
+	email = $git_email
+[branch "master"]
+	remote = origin
+	merge = refs/heads/master
+[core]
+	editor = vim
 
-  # Git colors
-  cat >> ~/.gitconfig <<EOF
 [color]
   branch = auto
   diff = auto
@@ -34,6 +36,7 @@ if [ -n "$git_name" ]; then
   changed = green
   untracked = cyan
 EOF
+chown $USER:$USER ~/.gitconfig
 
 else
   echo "Usage:  $0 <user name> <user email>"
