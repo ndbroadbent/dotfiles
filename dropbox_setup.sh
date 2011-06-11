ec710023523114c3db197f8c6728720fc935b97f
Nathan Broadbent <n>
2011-06-11 12:11:05 +0800

    Abstracted out dropbox and skype.

---
+++ dropbox_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+. _shared.sh
+# Install Dropbox Nautilus Extension
+# -------------------------------------------
+echo "== Installing dropbox nautilus extension..."
+
+if ! (apt-cache search nautilus-dropbox | grep -q nautilus-dropbox); then
+  # Dropbox source
+  echo "deb http://linux.dropbox.com/ubuntu $(lsb_release -cs) main" | sudo tee "/etc/apt/sources.list.d/dropbox.list" > /dev/null
+  sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
+fi
+
+apt_queue_or_install "nautilus-dropbox"
+
