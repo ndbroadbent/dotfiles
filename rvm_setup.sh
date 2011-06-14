a8ccbd1d11fc02be5b048b8ccd7b0477e3d100f3
Nathan Broadbent <nathan.f77@gmail.com>
2011-06-14 18:01:04 +0800

    no

--- rvm_setup.sh
+++ rvm_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z `which rvm` ]; then
   echo "== Installing rvm and ruby 1.9.2..."
-  sudo bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
+  bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
   [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"  # Loads RVM into shell
   rvm install 1.9.2
   rvm use 1.9.2
