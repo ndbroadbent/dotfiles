658649c528a2519d44c7e882def3460b14a3ba72
Nathan Broadbent <nathan.f77@gmail.com>
2011-06-15 11:36:42 +0800

    Fixed dodgy function

--- assets/bashrc/aliases.sh
+++ assets/bashrc/aliases.sh
@@ -16,7 +16,7 @@ alias ....='...;..'
 alias .....='....;..'
 alias ......='.....;..'
 alias n='nautilus .'
-function g() { gedit $1 &> /dev/null }
+function g() { gedit $1 &> /dev/null; }
 alias ak='ack-grep'
 
 # -- apt
