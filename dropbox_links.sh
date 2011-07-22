aa48f201acf10687f32b4ce9683fa17a74bac838
Nathan Broadbent <nathan.f77@gmail.com>
2011-07-22 12:32:52 +0800

    Remove links (if they exist) before re-creating them.

--- dropbox_links.sh
+++ dropbox_links.sh
@@ -4,6 +4,9 @@
 # ----------------------------------------------
 echo "== Creating Dropbox symlinks..."
 
+rm -f /home/$USER/Design
+rm -f /home/$USER/.ssh
+rm -f /home/$USER/.gtk-bookmarks
 ln -fs /home/$USER/Dropbox/Design /home/$USER/Design
 ln -fs /home/$USER/Dropbox/UbuntuSync/ssh /home/$USER/.ssh
 ln -fs /home/$USER/Dropbox/UbuntuSync/gtk-bookmarks /home/$USER/.gtk-bookmarks
