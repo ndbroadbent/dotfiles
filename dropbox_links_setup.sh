7cc622e4922744253ea3c48eb7ce6966d4f723e8
Nathan Broadbent <nathan.f77@gmail.com>
2011-08-05 10:30:05 +0800

    Firefox profile syncing was a pain! Different ffox versions cause a huge hassle, and ffox sync does it better anyway. Best solution: Siphon for add-ons, ffox sync for preferences, history, etc, and XMarks for bookmarks (can share with Chrome and IE).

--- dropbox_links_setup.sh
+++ dropbox_links_setup.sh
@@ -11,16 +11,3 @@ ln -fs /home/$USER/Dropbox/Design /home/$USER/Design
 ln -fs /home/$USER/Dropbox/UbuntuSync/ssh /home/$USER/.ssh
 ln -fs /home/$USER/Dropbox/UbuntuSync/gtk-bookmarks /home/$USER/.gtk-bookmarks
 
-
-# Sync Firefox profile (just extensions / preferences / etc)
-# ----------------------------------------------------------
-mkdir -p ~/.mozilla/firefox
-profile=$(grep "Path=" ~/.mozilla/firefox/profiles.ini | sed "s/Path=//g")
-if [ -n "$profile" ]; then
-  profile_path=/home/$USER/.mozilla/firefox/$profile
-  for file in /home/$USER/Dropbox/UbuntuSync/firefox_profile/*; do
-    rm -rf $profile_path/$(basename $file)
-    ln -fs $file $profile_path/$(basename $file)
-  done
-fi
-
