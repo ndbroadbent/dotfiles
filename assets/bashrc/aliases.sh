4b9961cfb688b03964d3a1d82a3389859bcfdd3f
Nathan Broadbent <nathan.f77@gmail.com>
2011-08-31 16:35:55 +0800

    rake() should call bundle exec even if just a Gemfile is present

--- assets/bashrc/aliases.sh
+++ assets/bashrc/aliases.sh
@@ -89,7 +89,7 @@ ensure_bundler() { if ! test $(which bundle); then gem install bundler; fi; }
 alias bi="ensure_bundler; bundle install"
 alias be="ensure_bundler; bundle exec"
 # Automatically invoke bundler for rake, if necessary.
-rake() { if [ -e ./Gemfile.lock ]; then bundle exec rake "$@"; else /usr/bin/env rake "$@"; fi; }
+rake() { if [ -e ./Gemfile ]; then bundle exec rake "$@"; else /usr/bin/env rake "$@"; fi; }
 
 # RVM ruby versions
 alias 192='rvm use ruby-1.9.2'
