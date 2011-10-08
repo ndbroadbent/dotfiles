dc8ac7557dd7fd28a12978dd7696af1e1422a227
Nathan Broadbent <nathan.f77@gmail.com>
2011-10-08 23:02:52 +0800

    Made 'gs' function super fast. (replaced 'echo > cut' with ${str:*:*}). Improved colors.

--- assets/bashrc/git.sh
+++ assets/bashrc/git.sh
@@ -97,130 +97,118 @@ ours(){   local files=$(git_expand_args "$@"); git checkout --ours $files; git a
 theirs(){ local files=$(git_expand_args "$@"); git checkout --theirs $files; git add $files; }
 
 
-# 'git status' wrapper
-# Processes git status output, exporting bash variables
+# 'git status' implementation
+# Processes 'git status --porcelain', exporting bash variables
 # for the filepaths of each modified file.
-# To ensure colored output, please run: $ git config --global color.status always
 # -----------------------------------------------------------
 gs() {
   export IFS=$'\n'
-  local status=`git status --porcelain`
+  local status=`git status --porcelain 2> /dev/null`
   local branch=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
-  local c_rst="\e[0m"
-  local c_branch="\e[1;37m"
 
-  # If status is blank
-  if [ -z "$status" ]; then
-    echo -e "# On branch: $c_branch$branch$c_rst\nnothing to commit (working directory clean)"
-  else
-    if [ $(echo "$status" | wc -l) -lt $gs_max_changes ]; then
-      unset stat_file; unset stat_col; unset stat_msg; unset stat_grp; unset stat_x; unset stat_y
-      # Colors
-      local c_header="\e[0m"
-      local c_brk="\e[2;37m"
+  if [ -n "$status" ] && [[ $(echo "$status" | wc -l) -lt $gs_max_changes ]]; then
+    unset stat_file; unset stat_col; unset stat_msg; unset stat_grp; unset stat_x; unset stat_y
+    # Colors
+    local c_rst="\e[0m"
+    local c_branch="\e[1;37m"
+    local c_header="\e[0m"
+    local c_brk="\e[2;37m"
 
-      local c_grp_1="\e[1;33m"
-      local c_grp_2="\e[1;31m"
-      local c_grp_3="\e[1;32m"
-      local c_grp_4="\e[1;34m"
+    local c_del="\e[0;31m"
+    local c_mod="\e[0;32m"
+    local c_new="\e[0;33m"
+    local c_ren="\e[0;34m"
+    local c_cpy="\e[0;33m"
+    local c_ign="\e[0;36m"
 
-      local c_del="\e[0;31m"
-      local c_mod="\e[0;32m"
-      local c_new="\e[0;33m"
-      local c_ren="\e[0;34m"
-      local c_cpy="\e[0;33m"
-      local c_ign="\e[0;36m"
+    # group headings
+    local c_head_1="\e[1;33m";
+    local c_head_2="\e[1;31m"
+    local c_head_3="\e[1;32m"
+    local c_head_4="\e[1;36m"
+    # group files
+    local c_grp_1="\e[0;33m";
+    local c_grp_2="\e[0;31m"
+    local c_grp_3="\e[0;32m"
+    local c_grp_4="\e[0;36m"
 
-      local f=0  # Counter for number of files
-      local e=1  # Counter for ENV variables
+    local f=0  # Counter for number of files
+    local e=1  # Counter for ENV variables
 
-      _gs_output_file_group() {
-        local output=""
-        for i in ${stat_grp[$1]}; do
-          output="$output\n"$(echo -e "#      ${stat_col[$i]}${stat_msg[$i]}:%\
-$c_brk[$c_rst$e$c_brk] $c_rst${stat_file[$i]}$c_rst")
-          # Export numbered variables in the order they are displayed.
-          export $git_env_char$e=${stat_file[$i]}
-          let e++
-        done
-        echo -e "$output" | column -t -s "%"
-        echo "#"
-      }
+    echo -e "# On branch: $c_branch$branch$c_rst  $c_brk|$c_rst  Shortcuts: $c_brk[$c_rst*$c_brk]$c_rst => \$$git_env_char*\n#"
 
-      echo -e "# On branch: $c_branch$branch$c_rst\n#"
+    for line in $status; do
+      let f++
+      # Process file state & deduce message, color and group.
+      # Groups => 1: staged, 2: unmerged, 3: unstaged, 4: untracked
+      case "${line:0:2}" in
+      M?) msg="modified";        col="$c_mod"; grp="1";;
+      A?) msg="new file";        col="$c_new"; grp="1";;
+      D?) msg="deleted";         col="$c_del"; grp="1";;
+      " M") msg="modified";      col="$c_mod"; grp="3";;
+      " D") msg="deleted";       col="$c_del"; grp="3";;
+      R?) msg="renamed";         col="$c_ren"; grp="3";;
+      C?) msg="copied";          col="$c_cpy"; grp="3";;
+      AA) msg="both added";      col="$c_new"; grp="2";;
+      AU) msg="added by us";     col="$c_new"; grp="2";;
+      DD) msg="both deleted";    col="$c_del"; grp="2";;
+      DU) msg="deleted by us";   col="$c_del"; grp="2";;
+      UD) msg="deleted by them"; col="$c_del"; grp="2";;
+      UA) msg="added by them";   col="$c_new"; grp="2";;
+      UU) msg="both modified";   col="$c_mod"; grp="2";;
+      "??") msg="untracked";     col="$c_ign"; grp="4";;
+      *) msg=""; col=""; grp="0";;
+      esac
+      # Store data in arrays
+      stat_file[$f]=${line:3}
+      stat_msg[$f]=$msg
+      stat_col[$f]=$col
+      # Add the file's index to the given group
+      stat_grp[$grp]="${stat_grp[$grp]} $f"
+    done
 
-      for line in $status; do
-        let f++
-        # Filenames
-        file=$(echo "$line" | sed "s/^.. //g")
-        x=$(echo "$line" | cut -c 1 )
-        y=$(echo "$line" | cut -c 2-2 )
-
-        # Store files & statuses in arrays
-        stat_file[$f]=$file
-        stat_x[$f]=$x
-        stat_y[$f]=$y
-
-        # Groups -- 1: staged, 2: unmerged, 3: unstaged, 4: untracked
-
-        # Generate status message
-        case "$x" in
-        M) msg="modified"; col="$c_mod"; grp="1";;
-        R) msg="renamed"; col="$c_ren"; grp="3";;
-        C) msg="copied"; col="$c_cpy"; grp="3";;
-        A) case "$y" in
-           A) msg="both added"; col="$c_new"; grp="2";;
-           U) msg="added by us"; col="$c_new"; grp="2";;
-           *) msg="new file"; col="$c_new"; grp="1";;
-           esac;;
-        D) case "$y" in
-           D) msg="both deleted"; col="$c_del"; grp="2";;
-           U) msg="deleted by us"; col="$c_del"; grp="2";;
-           *) msg="deleted"; col="$c_del"; grp="1";;
-           esac;;
-        U) case "$y" in
-           D) msg="deleted by them"; col="$c_del"; grp="2";;
-           A) msg="added by them"; col="$c_new"; grp="2";;
-           U) msg="both modified"; col="$c_mod"; grp="2";;
-          esac;;
-        " ") case "$y" in
-             M) msg="modified"; col="$c_mod"; grp="3";;
-             D) msg="deleted"; col="$c_del"; grp="3";;
-             *) msg="not updated"; col="$c_rst"; grp="0";;
-             esac;;
-        "?") msg="untracked"; col="$c_ign"; grp="4";;
-        esac
-        stat_msg[$f]=$msg; stat_col[$f]=$col
-        # add file to group
-        stat_grp[$grp]="${stat_grp[$grp]} $f"
-      done
-
-      export IFS=" "
-      if [ -n "${stat_grp[1]}" ]; then
-        echo -e "# $c_grp_1[$c_header Changes to be committed $c_grp_1]$c_rst\n#"
-        _gs_output_file_group 1
-      fi
-      if [ -n "${stat_grp[2]}" ]; then
-        echo -e "# $c_grp_2[$c_header Unmerged paths $c_grp_2]$c_rst\n#"
-        _gs_output_file_group 2
-      fi
-      if [ -n "${stat_grp[3]}" ]; then
-        echo -e "# $c_grp_3[$c_header Changes not staged for commit $c_grp_3]$c_rst\n#"
-        _gs_output_file_group 3
-      fi
-      if [ -n "${stat_grp[4]}" ]; then
-        echo -e "# $c_grp_4[$c_header Untracked files $c_grp_4]$c_rst\n#"
-        _gs_output_file_group 4
-      fi
-
-    else
-      # If there are too many changed files, this 'gs' function will slow down.
-      # In this case, fall back to plain 'git status'
-      git status
+    export IFS=" "
+    if [ -n "${stat_grp[1]}" ]; then
+      echo -e "# $c_head_1[$c_header Changes to be committed $c_head_1]$c_rst\n#"
+      _gs_output_file_group 1
     fi
+    if [ -n "${stat_grp[2]}" ]; then
+      echo -e "# $c_head_2[$c_header Unmerged paths $c_head_2]$c_rst\n#"
+      _gs_output_file_group 2
+    fi
+    if [ -n "${stat_grp[3]}" ]; then
+      echo -e "# $c_head_3[$c_header Changes not staged for commit $c_head_3]$c_rst\n#"
+      _gs_output_file_group 3
+    fi
+    if [ -n "${stat_grp[4]}" ]; then
+      echo -e "# $c_head_4[$c_header Untracked files $c_head_4]$c_rst\n#"
+      _gs_output_file_group 4
+    fi
+
+  else
+    # If there are too many changed files, this 'gs' function will slow down.
+    # In this case, fall back to plain 'git status'
+    git status
   fi
   # Reset IFS separator
   unset IFS
+}
+
+# Helper function for the 'gs' command.
+_gs_output_file_group() {
+  local output=""
+  local c_file="$(eval echo \$c_grp_$1)"
+  for i in ${stat_grp[$1]}; do
+    # Whitespace padding for [*]
+    if [[ $e -lt 10 ]]; then local pad=" "; else local pad=""; fi
+    output="$output\n"$(echo -e "#      ${stat_col[$i]}${stat_msg[$i]}:%\
+$pad$c_brk[$c_rst$e$c_brk] $c_file${stat_file[$i]}$c_rst")
+    # Export numbered variables in the order they are displayed.
+    export $git_env_char$e=${stat_file[$i]}
+    let e++
+  done
+  echo -e "$output" | column -t -s "%"
+  echo "#"
 }
 
 
