cab52ec3e211595a35c6bba730a142688e4a35a8
Nathan Broadbent <nathan.f77@gmail.com>
2011-10-12 12:53:03 +0800

    Added test for merge conflicts, fixed bug with status case. Added functions to turn on & off silent git commands.

--- test/bashrc/git_test.sh
+++ test/bashrc/git_test.sh
@@ -23,13 +23,22 @@ oneTimeSetUp() {
   testRepo="/tmp/testRepo"
 }
 
+# Silence output from git commands
+silentGitCommands() {
+  git() { /usr/bin/env git "$@" > /dev/null 2>&1; }
+}
+# Cancel silent override
+verboseGitCommands() {
+  unset git
+}
+
+
 setupTestRepo() {
   rm -rf "${testRepo}"
   mkdir -p "$testRepo"
   cd "$testRepo"
   git init > /dev/null
 }
-
 
 oneTimeTearDown() {
   rm -rf "${testRepo}"
@@ -49,8 +58,8 @@ assertIncludes() {
 }
 
 assertNotIncludes() {
-  result=0; if echo "$1" | grep -q "$2"; then result=1; fi
-  assertTrue "'$1' should not have contained '$2'" $result
+  result=1; if echo "$1" | grep -q "$2"; then result=0; fi
+  assertFalse "'$1' should not have contained '$2'" $result
 }
 
 sed_strip_colors="s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"
@@ -75,21 +84,25 @@ test_git_expand_args() {
 test_git_status_with_shortcuts() {
   setupTestRepo
 
+  silentGitCommands
+
   # Set up some modifications
   touch deleted_file
   git add deleted_file
-  git commit -m "Test commit" > /dev/null
+  git commit -m "Test commit"
   touch new_file
   touch untracked_file
   git add new_file
   echo "changed" > new_file
   rm deleted_file
 
-  # Test groups filtered by param
+  verboseGitCommands
+
+  # Test that groups can be filtered by passing a parameter
   status1=$(git_status_with_shortcuts 1)
   status3=$(git_status_with_shortcuts 3)
   status4=$(git_status_with_shortcuts 4)
-  # Test for presence of only expected groups
+  # Test for presence of expected groups
   assertIncludes "$status1" "Changes to be committed"
   assertIncludes "$status3" "Changes not staged for commit"
   assertIncludes "$status4" "Untracked files"
@@ -104,7 +117,7 @@ test_git_status_with_shortcuts() {
   temp_file=$(mktemp)
   git_status_with_shortcuts > $temp_file
 
-  # Test output without stripped color codes
+  # Test output with stripped color codes
   status=$(sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" $temp_file)
 
   assertIncludes "$status"  "new file: *\[1\] *new_file"       || return
@@ -112,6 +125,7 @@ test_git_status_with_shortcuts() {
   assertIncludes "$status"  "modified: *\[3\] *new_file"       || return
   assertIncludes "$status" "untracked: *\[4\] *untracked_file" || return
 
+  # Test that shortcut env variables are set
   local error="Env variable was not set"
   assertEquals "$error" "new_file" "$e1"       || return
   assertEquals "$error" "deleted_file" "$e2"   || return
@@ -119,7 +133,51 @@ test_git_status_with_shortcuts() {
   assertEquals "$error" "untracked_file" "$e4" || return
 }
 
-test_git_status_with_shortcuts_respect_max_changes() {
+test_git_status_with_shortcuts_merge_conflicts() {
+  setupTestRepo
+
+  silentGitCommands
+
+  # Set up every possible merge conflict
+  touch both_modified both_deleted deleted_by_them deleted_by_us
+  echo "renamed file needs some content" > renamed_file
+  git add both_modified both_deleted renamed_file deleted_by_them deleted_by_us
+  git commit -m "First commit"
+
+  git checkout -b conflict_branch
+  echo "added by branch" > both_added
+  echo "branch line" > both_modified
+  echo "deleted by us" > deleted_by_us
+  git rm deleted_by_them both_deleted
+  git mv renamed_file renamed_file_on_branch
+  git add both_added both_modified deleted_by_us
+  git commit -m "Conflict commit"
+
+  git checkout master
+  echo "added by master" > both_added
+  echo "master line" > both_modified
+  echo "deleted by them" > deleted_by_them
+  git rm deleted_by_us both_deleted
+  git mv renamed_file renamed_file_on_master
+  git add both_added both_modified deleted_by_them
+  git commit -m "Master commit"
+
+  git merge conflict_branch
+
+  verboseGitCommands
+
+  # Test output without stripped color codes
+  status=$(git_status_with_shortcuts | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g")
+  assertIncludes "$status"      "both added: *\[[0-9]*\] *both_added"             || return
+  assertIncludes "$status"   "both modified: *\[[0-9]*\] *both_modified"          || return
+  assertIncludes "$status" "deleted by them: *\[[0-9]*\] *deleted_by_them"        || return
+  assertIncludes "$status"   "deleted by us: *\[[0-9]*\] *deleted_by_us"          || return
+  assertIncludes "$status"    "both deleted: *\[[0-9]*\] *renamed_file"           || return
+  assertIncludes "$status"   "added by them: *\[[0-9]*\] *renamed_file_on_branch" || return
+  assertIncludes "$status"     "added by us: *\[[0-9]*\] *renamed_file_on_master" || return
+}
+
+test_git_status_with_shortcuts_max_changes() {
   setupTestRepo
 
   local gs_max_changes="5"
