4df429fe74b831341fa684fa3efb3c4d2606716c
Nathan Broadbent <nathan.f77@gmail.com>
2011-10-21 17:41:31 +0800

    Changed project design folder to design_assets

--- assets/bashrc/design.sh
+++ assets/bashrc/design.sh
@@ -3,7 +3,7 @@
 # Written by Nathan Broadbent (www.madebynathan.com)
 # -------------------------------------------------------
 #
-# * The `design` function manages the 'Design' directory tree for the current project,
+# * The `design` function manages the 'design_assets' directory tree for the current project,
 #   including folders such as Backgrounds, Logos, Icons, and Samples. The actual directories are
 #   created in $design_dir, symlinked into the project, and ignored from source control.
 #   This is because we usually don't want to check in large bitmaps or wav files into our code repository,
@@ -11,6 +11,7 @@
 #
 # Examples:
 #
+#    $ design link        # Requires SCM Breeze - Links existing design directories into each of your projects
 #    $ design init        # Creates default directory structure at $design_dir/**/ubuntu_config and symlinks into project.
 #                           (Backgrounds Logos Icons Mockups Screenshots)
 #    $ design init --av   # Adds extra directories for audio/video assets
@@ -25,12 +26,12 @@ export design_dir="$HOME/Design"
 
 # Add ignore rule to .git/info/exclude if not already present
 _design_add_git_exclude(){
-  if ! $(touch "$1/.git/info/exclude" && cat "$1/.git/info/exclude" | grep -q "Design"); then
-    echo "Design" >> "$1/.git/info/exclude"
+  if ! $(touch "$1/.git/info/exclude" && cat "$1/.git/info/exclude" | grep -q "design_assets"); then
+    echo "design_assets" >> "$1/.git/info/exclude"
   fi
 }
 
-# Manage 'Design' directories for project.
+# Manage 'design_assets' directories for project.
 design() {
   local project=`basename $(pwd)`
   local int_dirs="_Fonts _IconSets"
@@ -57,11 +58,11 @@ design() {
     if [ "$1" = "init" ]; then
       if [ "$2" = "--av" ]; then base_dirs+=" $av_dirs"; fi
       echo "Creating the following design directories for $project: $base_dirs"
-      mkdir -p Design
+      mkdir -p design_assets
       # Create and symlink each directory
       for dir in $base_dirs; do
         mkdir -p "$design_dir/$dir/$project"
-        if [ ! -e ./Design/$dir ]; then ln -sf "$design_dir/$dir/$project" Design/$dir; fi
+        if [ ! -e ./design_assets/$dir ]; then ln -sf "$design_dir/$dir/$project" design_assets/$dir; fi
       done
       _design_add_git_exclude $PWD
 
@@ -73,11 +74,11 @@ design() {
           proj=$(basename $design_path)
           repo_path=$(grep "/$proj$" $GIT_REPO_DIR/.git_index)
           if [ -n "$repo_path" ]; then
-            mkdir -p "$repo_path/Design"
-            if [ -e "$repo_path/Design/*" ]; then rm $repo_path/Design/*; fi
+            mkdir -p "$repo_path/design_assets"
+            if [ -e "$repo_path/design_assets/*" ]; then rm $repo_path/design_assets/*; fi
             _design_add_git_exclude $repo_path
-            if ! [ -e "$repo_path/Design/$dir" ]; then ln -fs "$design_path" "$repo_path/Design/$dir"; fi
-            echo "=> $repo_path/Design/$dir"
+            if ! [ -e "$repo_path/design_assets/$dir" ]; then ln -fs "$design_path" "$repo_path/design_assets/$dir"; fi
+            echo "=> $repo_path/design_assets/$dir"
           fi
         done
       done
@@ -87,7 +88,7 @@ design() {
       echo "Removing all design directories for $project..."
       base_dirs+=" $av_dirs"
       for dir in $base_dirs; do rm -rf "$design_dir/$dir/$project"; done
-      rm -rf Design
+      rm -rf design_assets
 
     elif [ "$1" = "trim" ]; then
       echo "Trimming empty design directories for $project..."
@@ -95,7 +96,7 @@ design() {
       for dir in $base_dirs; do
         if ! [ -e "$design_dir/$dir/$project/*" ]; then
           rm -rf "$design_dir/$dir/$project"
-          rm -f Design/$dir
+          rm -f design_assets/$dir
         fi
       done
 
