097795a92d2003ca040a1b9374e2c44215d52b8f
Nathan Broadbent <nathan.f77@gmail.com>
2011-10-21 23:59:09 +0800

    design has moved to a new home at SCM Breeze

--- bashrc_setup.sh
+++ bashrc_setup.sh
@@ -4,7 +4,7 @@ this_dir=$(pwd)
 
 # Assemble bashrc from parts
 cat assets/bashrc/default.sh > ~/.bashrc
-for part in prompt history aliases functions ruby_on_rails crossroads design; do
+for part in prompt history aliases functions ruby_on_rails crossroads; do
   cat assets/bashrc/$part.sh >> ~/.bashrc
 done
 
