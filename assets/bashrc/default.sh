40211ad697cbc4372289a8f82875418aa4bb823a
Nathan Broadbent <nathan.f77@gmail.com>
2011-11-17 10:01:43 +0800

    Added user path

--- assets/bashrc/default.sh
+++ assets/bashrc/default.sh
@@ -71,6 +71,8 @@ if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
     . /etc/bash_completion
 fi
 
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then PATH="$HOME/bin:$PATH"; fi
 
 # This loads RVM into the shell session.
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" #]
