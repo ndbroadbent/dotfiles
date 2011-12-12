ad3923e75d37d1b53de2d0293d172d8433118ae5
Nathan Broadbent <nathan.f77@gmail.com>
2011-12-12 09:13:41 +0800

    Fixed startup apps

--- startup_setup.sh
+++ startup_setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # The following programs will run on system startup
 
-echo "== Configuring firefox, terminal & gedit to run on startup..."
+echo "== Configuring chrome, terminal & geany to run on startup..."
 
 # Create startup script including delay (wait for panels to be shown, otherwise app title bar is hidden.)
 cat > ~/.start_dev_applications.sh <<EOF
 #!/bin/sh
 sleep 20
 gnome-terminal &
-firefox &
-gedit &
+google-chrome &
+geany &
 EOF
 chmod +x ~/.start_dev_applications.sh
 
