191d1c85ca7cbbda218ba54ff83bdcd8a9954a57
Nathan Broadbent <nathan.f77@gmail.com>
2012-01-17 10:16:59 +0800

    Updated git setup scripts, refactored prompt_for_git function to skip if already configured

--- setup/git_config.sh
+++ setup/git_config.sh
@@ -3,21 +3,11 @@ if [ "$(basename $(pwd))" = "setup" ]; then . _shared.sh; else . setup/_shared.s
 
 # If name was passed in ENV variable, use it
 if [ -z "$git_name" ]; then
-  # Check for existing git config
-  git_name=$(git config --get user.name)
-  git_email=$(git config --get user.email)
-  github_user=$(git config --get github.user)
-  github_token=$(git config --get github.token)
-  
-  # If still no name, prompt.
-  if [ -z "$git_name" ] || [ -z "$git_email" ]; then
-    prompt_for_git
-  fi
+  prompt_for_git
 fi
 
 echo "== Setting up git config..."
-echo "   name: '$git_name',  email: '$git_email' "
-echo "   github user: '$github_user',  github token: '$github_token'"
+
 # Add user to gitconfig
 cat > ~/.gitconfig <<EOF
 [user]
