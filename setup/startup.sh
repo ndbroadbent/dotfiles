c7424c5a9554c668667d7a5c51fe221d6b48afa8
Nathan Broadbent <nathan.f77@gmail.com>
2012-01-19 14:07:21 +0800

    Fixed startup script

--- setup/startup.sh
+++ setup/startup.sh
@@ -6,10 +6,10 @@ echo "== Configuring chrome, terminal & geany to run on startup..."
 # Create startup script including delay (wait for panels to be shown, otherwise app title bar is hidden.)
 cat > ~/.start_dev_applications.sh <<EOF
 #!/bin/sh
-sleep 20
-if which gnome-terminal; then gnome-terminal &; else xfce4-terminal &; fi
+sleep 15
 google-chrome &
 geany &
+if which gnome-terminal; then \$(gnome-terminal &); else \$(xfce4-terminal &); fi
 EOF
 chmod +x ~/.start_dev_applications.sh
 
