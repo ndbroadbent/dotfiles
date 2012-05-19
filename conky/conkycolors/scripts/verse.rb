ba877ee8c5033235b357bd6d8c866ebc6c17908c
Nathan Broadbent <nathan.f77@gmail.com>
2012-05-20 09:28:56 +1200

    Fixed verse script

--- conky/conkycolors/scripts/verse.rb
+++ conky/conkycolors/scripts/verse.rb
@@ -1,17 +1,13 @@
 #!/usr/bin/env ruby
 # Formats output of 'verse' nicely.
 verse = `verse`
-ref = verse[Regexp.new('"  +(.*)$', Regexp::MULTILINE),1].to_s.strip
+ref = verse[/"[\n ]+(.*)$/m,1].to_s.strip
 verse = verse.gsub(ref, "").strip
-verse=verse.gsub(/  +/," ").gsub("\n","").scan(/.{1,30}/).
-  join("\n<br>").gsub(/([^ ]*)\n<br>/m,"\n"<<"\\1").strip
-ref=ref.strip
-case ARGV.first
-when "ref"
-  puts ref
-when "verse"
-  puts verse
-else
-  puts "#{verse}\n#{ref}"
+verse = verse.gsub(/  +/," ").gsub("\n","").scan(/.{1,30}/).
+        join("\n<br>").gsub(/([^ ]*)\n<br>/m,"\n"<<"\\1").strip
+puts case ARGV.first
+when "ref"; ref
+when "verse"; verse
+else "#{ref}\n#{verse}"
 end
 
