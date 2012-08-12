4cefe0853b5869dbadff9edd8a3372f86fc22afe
Nathan Broadbent <nathan.f77@gmail.com>
2012-08-12 22:48:33 +1200

    Fixed issue with spaces in paths

--- bashrc/functions.sh
+++ bashrc/functions.sh
@@ -16,8 +16,8 @@ gsed() {
 find_in_cwd_or_parent() {
   local slashes=${PWD//[^\/]/}; local directory=$PWD;
   for (( n=${#slashes}; n>0; --n )); do
-    test -e $directory/$1 && echo "$directory/$1" && return 0
-    directory=$directory/..
+    test -e "$directory/$1" && echo "$directory/$1" && return 0
+    directory="$directory/.."
   done
   return 1
 }
