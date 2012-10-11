66ae3c2f20537242b6a966efbfd47aec19616bf3
Nathan Broadbent <nathan.f77@gmail.com>
2012-10-11 15:08:47 +1300

    Added git_index job type for whenever schedule

--- schedule.rb
+++ schedule.rb
@@ -3,10 +3,17 @@
 
 # Install this crontab with: whenever -f schedule.rb -w
 
+# 'Git index' jobs require SCM Breeze: https://github.com/ndbroadbent/scm_breeze
+job_type :git_index, <<-CMD.gsub(/\s{2,}/, ' ').strip
+  source #{ENV['HOME']}/.scm_breeze/scm_breeze.sh;
+  git_index --:task
+CMD
+
+
 every 30.minutes do
   # Fetch all remotes for indexed git repos, and fast-forward if possible
   # Send notifications using notify-send
-  command "source #{ENV['HOME']}/.bashrc; export NOTIFY=true; git_index --update-all"
+  git_index "update-all-with-notifications"
 end
 
 every 10.minutes do
@@ -25,16 +32,16 @@ end
 
 # Rebuild SCM Breeze index
 every 1.minute do
-  command "source #{ENV['HOME']}/.bashrc; git_index --rebuild"
+  git_index "rebuild"
 end
 
 # Update Travis CI build statuses for current branch of indexed git repos
 every 30.minutes do
-  command "source #{ENV['HOME']}/.bashrc; git_index --batch-cmd update_travis_ci_status"
+  git_index "batch-cmd update_travis_ci_status"
 end
 
 # Install gem dependencies via Bundler, for all indexed repos that contain a Gemfile.
 every 1.hour do
-  command "source #{ENV['HOME']}/.bashrc; git_index --batch-cmd bundle_check_or_install"
+  git_index "batch-cmd bundle_check_or_install"
 end
 
