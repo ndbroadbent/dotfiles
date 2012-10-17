d32d39fedb4317c507c65ec33015965cb85d7bd0
Nathan Broadbent <nathan.f77@gmail.com>
2012-10-17 18:11:51 +1300

    geany => sublime_text_2

--- setup/startup.sh
+++ setup/startup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # The following programs will run on system startup
 
-echo "== Configuring chrome, terminal & geany to run on startup..."
+echo "== Configuring chrome, terminal & sublime_text_2 to run on startup..."
 
 # Create startup script including delay (wait for panels to be shown, otherwise app title bar is hidden.)
 cat > ~/.start_dev_applications.sh <<EOF
 #!/bin/sh
 google-chrome &
-geany &
+sublime_text_2 &
 if which gnome-terminal > /dev/null; then \$(gnome-terminal &); else \$(xfce4-terminal &); fi
 EOF
 chmod +x ~/.start_dev_applications.sh
