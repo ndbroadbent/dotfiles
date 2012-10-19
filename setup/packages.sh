c72080215273d82ebc5f3c346d155bfcd87d1566
Nathan Broadbent <nathan.f77@gmail.com>
2012-10-20 09:47:39 +1300

    Updated packages

--- setup/packages.sh
+++ setup/packages.sh
@@ -4,7 +4,12 @@ echo "== Setting up default software packages..."
 
 # -----------------------------------------------------------------------------------
 # Base
-pkg_base="curl vim htop ack-grep xclip ssh synergy apache2 build-essential"
+pkg_base="curl vim htop tmux ack-grep xclip ssh synergy"
+
+# Ruby requirements
+pkg_ruby="build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev \
+          libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev \
+          ncurses-dev automake libtool bison subversion pkg-config"
 
 # Development
 pkg_dev="git-core git-gui gitk tig python"
@@ -15,16 +20,12 @@ pkg_db="postgresql libpq-dev mysql-client libmysqlclient-dev sqlite3 libsqlite3-
 # Gnome (OS UI)
 pkg_gnome="libnotify-bin python-pyinotify python-webkit python-webkit-dev"
 
-# Addons / Tweaks
-pkg_addons="nautilus-image-converter nautilus-open-terminal nautilus-gksu"
-
 # Applications
-pkg_apps="vlc thunderbird"
+pkg_apps="vlc"
 
 # -----------------------------------------------------------------------------------
 # Queue or install apt packages
-apt_queue_or_install "$pkg_base $pkg_dev $pkg_gnome $pkg_addons $pkg_apps"
+apt_queue_or_install "$pkg_base $pkg_ruby $pkg_dev $pkg_gnome $pkg_apps"
 
 # Add Canonical Partners repository
 sudo add-apt-repository "deb http://archive.canonical.com/ $ubuntu_codename partner"
-
