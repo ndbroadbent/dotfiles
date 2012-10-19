3f99e6f35571b891684ea3677a77538946884004
Nathan Broadbent <nathan.f77@gmail.com>
2012-10-20 12:51:09 +1300

    Move ssh symlinks into dropbox_symlinks.sh

--- setup/dropbox_symlinks.sh
+++ setup/dropbox_symlinks.sh
@@ -5,8 +5,13 @@ if [ "$(basename $(pwd))" = "setup" ]; then . _shared.sh; else . setup/_shared.s
 echo "== Symlinking shared Ubuntu folders from Dropbox..."
 
 rm -rf $HOME/Design
-rm -rf $HOME/.ssh
 rm -rf $HOME/.gtk-bookmarks
 ln -fs $HOME/Dropbox/Design $HOME/Design
-ln -fs $HOME/Dropbox/UbuntuSync/ssh $HOME/.ssh
 ln -fs $HOME/Dropbox/UbuntuSync/gtk-bookmarks $HOME/.gtk-bookmarks
+
+# Symlinks SSH configuration files from Dropbox
+mkdir -p $HOME/.ssh/
+for f in $HOME/Dropbox/UbuntuSync/ssh/*; do
+  rm -f "$HOME/.ssh/$(basename $f)"
+  ln -fs "$f" "$HOME/.ssh/"
+done
