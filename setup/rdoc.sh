6bc8e919d08df3d3a5fb5598194b9ec089fd9d59
Nathan Broadbent <nathan.f77@gmail.com>
2012-10-20 09:51:23 +1300

    Not needed (replaced by sdoc local project)

--- setup/rdoc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rdoc_version=rails-v3.0.8rc1_ruby-v1.9.2
-doc_path=/usr/share/doc/rdoc-$rdoc_version
-
-wget http://railsapi.com/doc/$rdoc_version/rdoc.zip -O /tmp/$rdoc_version.zip
-sudo unzip /tmp/$rdoc_version.zip -d $doc_path
-# Open in firefox
-firefox $doc_path/index.html &
-
