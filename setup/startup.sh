44e3ae102bc41b5d36d860a6eaede699b0aed449
Nathan Broadbent <nathan.f77@gmail.com>
2012-10-29 18:17:15 +1300

    Start chrome after 5 seconds so that Wifi is connected

--- setup/startup.sh
+++ setup/startup.sh
@@ -6,9 +6,9 @@ echo "== Configuring chrome, terminal & sublime_text_2 to run on startup..."
 # Create startup script including delay (wait for panels to be shown, otherwise app title bar is hidden.)
 cat > ~/.start_dev_applications.sh <<EOF
 #!/bin/sh
-google-chrome &
 sublime_text_2 &
 if which gnome-terminal > /dev/null; then \$(gnome-terminal &); else \$(xfce4-terminal &); fi
+sleep 5 && google-chrome &
 EOF
 chmod +x ~/.start_dev_applications.sh
 
