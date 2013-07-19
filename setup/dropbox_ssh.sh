d04f534841aff5499922dec58aa7c3cbbcce7a55
Nathan Broadbent <nathan.f77@gmail.com>
2013-07-19 14:58:03 -0700

    Fixed SSH setup script

--- setup/dropbox_ssh.sh
+++ setup/dropbox_ssh.sh
@@ -2,7 +2,10 @@
 if [ "$(basename $(pwd))" = "setup" ]; then . _shared.sh; else . setup/_shared.sh; fi;
 echo "== Decrypting .ssh from Dropbox..."
 
-openssl aes-256-cbc -d -in $HOME/Dropbox/ssh.tar.gz.encrypted -out $HOME/ssh.tar.gz
+openssl aes-256-cbc -d -in $HOME/Dropbox/UbuntuSync/ssh.tar.gz.encrypted -out /tmp/ssh.tar.gz
 
-cd $HOME
+cd /tmp
 tar xvf ssh.tar.gz
+mkdir -p $HOME/.ssh
+cp -R /tmp/home/ndbroadbent/.ssh/* ~/.ssh/
+rm -rf /tmp/ssh.tar.gz /tmp/home/ndbroadbent/.ssh/ 
