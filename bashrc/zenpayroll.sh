25b97d6808a3da2b1007995c1d14667cd813835a
Nathan Broadbent <nathan.f77@gmail.com>
2013-07-22 20:33:04 -0700

    Updated ZP bashrc stuff

--- bashrc/zenpayroll.sh
+++ bashrc/zenpayroll.sh
@@ -1,5 +1,6 @@
 #export STE_DRB_URI=druby://localhost:3777
 # Add arcanist/bin to PATH
-PATH="$PATH:~/code/php/arcanist/bin"
+PATH="$PATH:~/code/zenpayroll/arcanist/bin"
 
 LD_LIBRARY_PATH="/usr/local/lib"
+export STE_DRB_URI="druby://localhost:3777"
