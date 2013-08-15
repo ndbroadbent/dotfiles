6dcbffd45c60dc07d1e9c3d389c53a7e9fb2c0c3
Nathan Broadbent <nathan.f77@gmail.com>
2013-08-15 15:41:54 -0700

    Added ZPDEBUG vars

--- bashrc/zenpayroll.sh
+++ bashrc/zenpayroll.sh
@@ -5,3 +5,6 @@ PATH="$PATH:~/code/zenpayroll/arcanist/bin"
 LD_LIBRARY_PATH="/usr/local/lib"
 export STE_DRB_URI="druby://localhost:3777"
 export RUBYLIB=${RUBYLIB}:/Applications/RubyMine.app/rb/testing/patch/bdd:/Applications/RubyMine.app/rb/testing/patch/common
+
+export ZPDEBUG_EDITOR_PATH=x-mine://open?file=/Users/ndbroadbent/code/zenpayroll/zenpayroll/
+export ZPDEBUG_ENABLED=1
