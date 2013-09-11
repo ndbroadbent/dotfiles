ea4d8075859f7de3a8f1fb81ee903a8a6a50b139
Nathan Broadbent <nathan.f77@gmail.com>
2013-09-10 20:17:51 -0700

    Added zpr alias to refresh Rails code

--- bashrc/zenpayroll.sh
+++ bashrc/zenpayroll.sh
@@ -10,3 +10,6 @@ export ZPDEBUG_EDITOR_PATH=x-mine://open?file=/Users/ndbroadbent/code/zenpayroll
 export ZPDEBUG_ENABLED=1
 
 alias vste="pushd ~/code/zenpayroll; vagrant up; vagrant ssh -c \"\\cd /vagrant/ste && ./bin/ste_drb &\"; popd"
+
+# Reload ZP code (AJAX requests don't reload code)
+alias zpr="curl http://zenpayroll.dev:3000 > /dev/null"
