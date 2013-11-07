dc34821d5be88a9e33001e4d2453f654b0c33947
Nathan Broadbent <nathan.f77@gmail.com>
2013-11-06 20:28:32 -0800

    Update irbrc and zp bashrc

--- bashrc/zenpayroll.sh
+++ bashrc/zenpayroll.sh
@@ -3,7 +3,8 @@
 PATH="$PATH:~/code/zenpayroll/arcanist/bin"
 
 LD_LIBRARY_PATH="/usr/local/lib"
-export STE_DRB_URI="druby://localhost:3777"
+#export STE_DRB_URI="druby://localhost:3777"
+export STE_DRB_URI="druby://leeroy:3777"
 export RUBYLIB=${RUBYLIB}:/Applications/RubyMine.app/rb/testing/patch/bdd:/Applications/RubyMine.app/rb/testing/patch/common
 
 export ZPDEBUG_EDITOR_PATH=x-mine://open?file=/Users/ndbroadbent/code/zenpayroll/zenpayroll/
