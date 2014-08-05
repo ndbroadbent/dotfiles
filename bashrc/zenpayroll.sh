d95157d26dccab57974510c9aa2ef4a8623a39c4
Nathan Broadbent <nathan.f77@gmail.com>
2014-08-04 17:02:12 -0700

    added zp aliases

--- bashrc/zenpayroll.sh
+++ bashrc/zenpayroll.sh
@@ -10,10 +10,15 @@ export RUBYLIB=${RUBYLIB}:/Applications/RubyMine.app/rb/testing/patch/bdd:/Appli
 export ZPDEBUG_EDITOR_PATH=x-mine://open?file=/Users/ndbroadbent/code/zenpayroll/zenpayroll/
 export ZPDEBUG_ENABLED=1
 
+export ZPDEBUG_LIVE_EDIT=1
+export ZPDEBUG_LIVE_EDIT_PORT=1337
+
 alias vste="pushd ~/code/zenpayroll; vagrant up; vagrant ssh -c \"\\cd /vagrant/ste && ./bin/ste_drb &\"; popd"
 
 # Reload ZP code (AJAX requests don't reload code)
 alias zpr="curl http://zenpayroll.dev:3000 > /dev/null"
+
+alias z="cd ~/code/zenpayroll/zenpayroll"
 
 export CAPISTRANO_STAGES="staging sandbox production"
 
