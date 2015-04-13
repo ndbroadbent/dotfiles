6671434168bb3abea16e0bcf55b8e2c14ac67b8c
Nathan Broadbent <nathan.f77@gmail.com>
2015-04-13 17:31:09 +0700

    Updated RVM install script

--- setup/rvm.sh
+++ setup/rvm.sh
@@ -3,15 +3,15 @@ if [ "$(basename $(pwd))" = "setup" ]; then . _shared.sh; else . setup/_shared.s
 apt_add_dependency "curl"
 
 # Install required Ruby Libraries
-apt_queue_or_install "libyaml-ruby libzlib-ruby libreadline-dev libruby \
+apt_queue_or_install "libreadline-dev libruby \
 imagemagick libmagickwand-dev libmagickcore-dev libxslt1-dev libxml2-dev"
 
 if [ -z `which rvm` ]; then
   echo "== Installing rvm and ruby 1.9.2..."
-  bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
+  \curl -sSL https://get.rvm.io | bash -s stable --ruby
   [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"  # Loads RVM into shell
-  rvm install 1.9.2
-  rvm use 1.9.2 --default
 else
   echo "== Updating rvm..."
   rvm get stable
