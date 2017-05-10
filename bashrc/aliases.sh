949b85fecc4380e269f0147b0a0565d9ea69826b
Nathan Broadbent <git@ndbroadbent.com>
2017-05-11 01:09:25 +0700

    Added more logging to clipboard aliases

--- bashrc/aliases.sh
+++ bashrc/aliases.sh
@@ -92,11 +92,11 @@ alias cb="simple_clipboard"
 # Copy contents of a file
 alias cbf="simple_clipboard <"
 # Copy SSH public key
-alias cbs="simple_clipboard < ~/.ssh/id_rsa.pub"
+alias cbs="echo 'Copying ~/.ssh/id_rsa.pub to clipboard...' && simple_clipboard < ~/.ssh/id_rsa.pub"
 # Copy current working directory
 alias cbd="pwd | simple_clipboard"
 # Copy current git SHA-1
-alias cbg="git rev-parse --verify HEAD | simple_clipboard"
+alias cbg="echo 'Copying latest commit hash to clipboard...' && git rev-parse --verify HEAD | simple_clipboard"
 
 # Apt
 # -------------------------------------------------
