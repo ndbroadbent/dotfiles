05a10e76d9c8c6792d2d8ac42be5b8817ea22c02
Nathan Broadbent <git@ndbroadbent.com>
2018-09-23 20:56:52 +0700

    Fix up some more bashrc stuff

--- bashrc/golang.sh
+++ bashrc/golang.sh
@@ -4,4 +4,5 @@
 # export GOBIN="$HOME/go/bin"
 # export PATH="$PATH:$GOBIN"
 export GOPATH="/Users/ndbroadbent/code/go"
-export PATH="/usr/local/opt/go@1.9/bin:$GOPATH/bin:$PATH"
+# export PATH="/usr/local/opt/go@1.9/bin:$GOPATH/bin:$PATH"
+export PATH="/usr/local/opt/go/libexec/bin:$GOPATH/bin:$PATH"
