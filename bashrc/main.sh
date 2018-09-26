39730ce03fcf47a56a03678cb01e298b8a9eec41
Nathan Broadbent <git@ndbroadbent.com>
2018-09-27 02:01:04 +0700

    Move most of the bashrc into this repo, instead of generating it

---
+++ bashrc/main.sh
@@ -0,0 +1,61 @@
+BASHRC_MODULES=" \
+  auto_reload
+  default
+  functions
+  ip
+  aliases
+  convox
+  docker
+  ruby_on_rails
+  capistrano
+  golang
+  java
+  android
+  database_branches
+  tmux
+  prompt
+  path
+"
+
+# DEBUG_BASHRC=true
+
+for BASHRC_MODULE in $BASHRC_MODULES; do
+  [[ -n $DEBUG_BASHRC ]] && echo "Loading ${BASHRC_MODULE}..."
+  source "${DOTFILES_PATH}/bashrc/${BASHRC_MODULE}.sh"
+done
+unset BASHRC_MODULE
+SOURCED_FILES="$SOURCED_FILES $DOTFILES_PATH/bashrc/main.sh"
+
+[[ -n $DEBUG_BASHRC ]] && echo "Loading Google Cloud SDK..."
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '~/google-cloud-sdk/path.bash.inc' ]; then
+  source '~/google-cloud-sdk/path.bash.inc'
+fi
+# The next line enables shell command completion for gcloud.
+if [ -f '~/google-cloud-sdk/completion.bash.inc' ]; then
+  source '~/google-cloud-sdk/completion.bash.inc'
+fi
+
+# NVM
+[[ -n $DEBUG_BASHRC ]] && echo "Loading NVM..."
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
+
+# RVM
+[[ -n $DEBUG_BASHRC ]] && echo "Loading RVM..."
+[ -s "$HOME/.rvm/scripts/rvm" ] && source "$HOME/.rvm/scripts/rvm"
+
+# SCM Breeze
+[[ -n $DEBUG_BASHRC ]] && echo "Loading SCM Breeze..."
+[ -s "$HOME/.scm_breeze/scm_breeze.sh" ] && source "$HOME/.scm_breeze/scm_breeze.sh"
+
+# Rails Shell
+[[ -n $DEBUG_BASHRC ]] && echo "Loading Rails Shell..."
+source "$DOTFILES_PATH/rails_shell/rails_shell.sh"
+
+# Finalize auto_reload sourced files
+[[ -n $DEBUG_BASHRC ]] && echo "Finalizing Bash autoreload..."
+finalize_auto_reload
+
+unset DEBUG_BASHRC
--- setup/bashrc.sh
+++ setup/bashrc.sh
@@ -2,98 +2,14 @@
 echo "== Installing ~/.bashrc ..."
 this_dir=$(pwd)
 
-IFS=" "
-
-bashrc_parts=" \
-  auto_reload \
-  default \
-  functions \
-  ip \
-  aliases \
-  convox \
-  docker \
-  tab_completion \
-  ruby_on_rails \
-  capistrano \
-  golang \
-  java \
-  android \
-  database_branches \
-  tmux \
-  prompt"
-
 # Header
 cat > ~/.bashrc <<EOF
 # Export path of dotfiles repo
 export DOTFILES_PATH="$this_dir"
-
+source "\$DOTFILES_PATH/bashrc/main.sh"
 EOF
-
-for part in $bashrc_parts; do
-  if [ "$1" = "copy" ]; then
-    # Assemble bashrc from parts
-    cat assets/bashrc/$part.sh >> ~/.bashrc
-  else
-    # Source bashrc from parts
-    echo "source \"\$DOTFILES_PATH/bashrc/$part.sh\"" >> ~/.bashrc
-  fi
-done
-echo >> ~/.bashrc
-
-# Footer
-cat >> ~/.bashrc <<EOF
-# NVM (Node)
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# YARN (Node)
-export PATH="\$PATH:\$HOME/.yarn/bin"
-
-# NPM
-export PATH="\$PATH:./node_modules/.bin"
-
-# RVM (Ruby)
-[ -s "\$HOME/.rvm/scripts/rvm" ] && source "\$HOME/.rvm/scripts/rvm"
-export PATH="\$PATH:\$HOME/.rvm/bin" # Add RVM to PATH for scripting
-
-# SCM Breeze
-[ -s "\$HOME/.scm_breeze/scm_breeze.sh" ] && source "\$HOME/.scm_breeze/scm_breeze.sh"
-
-# Hub tab completion
-[ -s "\$HOME/.hub.bash_completion" ] && source "\$HOME/.hub.bash_completion"
-
-# Rails Shell
-source "\$DOTFILES_PATH/rails_shell/rails_shell.sh"
-
-# GNU utils
-PATH="\$PATH:/usr/local/opt/coreutils/libexec/gnubin"
-
-# Dotfiles Scripts
-export PATH="\$PATH:$DOTFILES_PATH/bin"
-
-# System Python
-export PATH="\$PATH:$HOME/Library/Python/2.7/bin/"
-
-# Node modules in current dir
-export PATH="\$PATH:node_modules/.bin"
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/ndbroadbent/google-cloud-sdk/path.bash.inc' ]; then source '/Users/ndbroadbent/google-cloud-sdk/path.bash.inc'; fi
-
-# The next line enables shell command completion for gcloud.
-if [ -f '/Users/ndbroadbent/google-cloud-sdk/completion.bash.inc' ]; then source '/Users/ndbroadbent/google-cloud-sdk/completion.bash.inc'; fi
-
-export PATH="\$PATH:/Users/ndbroadbent/anaconda/bin"
-export PATH="\$PATH:/usr/local/opt/imagemagick@6/bin"
-export PATH="\$PATH:\$(brew --prefix qt@5.5)/bin"
-
-# Finalize auto_reload sourced files
-finalize_auto_reload
-EOF
-
 
 # If this script was sourced from the terminal, update current shell
-if ! [[ "$0" =~ "dev_machine_setup.sh" ]] && [[ "$0" == *bash ]]; then source ~/.bashrc; fi
-
-IFS=$' \t\n'
+if ! [[ "$0" =~ "dev_machine_setup.sh" ]] && [[ "$0" == *bash ]]; then
+  source ~/.bashrc
+fi
