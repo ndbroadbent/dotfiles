c67ddc57df9c7f4d42a4d61cae1a9bd62fcc5f65
Nathan Broadbent <git@ndbroadbent.com>
2018-09-27 15:53:43 +0700

    Re-add the missing PATH stuff that I accidentally deleted

--- bashrc/android.sh
+++
@@ -1,3 +0,0 @@
-export ANDROID_HOME=${HOME}/Library/Android/sdk
-export PATH=${PATH}:${ANDROID_HOME}/tools
-export PATH=${PATH}:${ANDROID_HOME}/platform-tools
---
+++ bashrc/path.sh
@@ -0,0 +1,30 @@
+# Android
+export ANDROID_HOME=${HOME}/Library/Android/sdk
+export PATH="${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools"
+
+# YARN (Node)
+export PATH="${PATH}:${HOME}/.yarn/bin"
+
+# NPM
+export PATH="${PATH}:./node_modules/.bin"
+
+# RVM (Ruby)
+export PATH="${PATH}:${HOME}/.rvm/bin"
+
+# GNU utils
+export PATH="${PATH}:/usr/local/opt/coreutils/libexec/gnubin"
+
+# Dotfiles Scripts
+export PATH="${PATH}:${DOTFILES_PATH}/bin"
+
+# System Python
+export PATH="${PATH}:${HOME}/Library/Python/2.7/bin/"
+
+# Node modules in current dir
+export PATH="${PATH}:node_modules/.bin"
+
+export PATH="${PATH}:/Users/ndbroadbent/anaconda/bin"
+export PATH="${PATH}:/usr/local/opt/imagemagick@6/bin"
+
+# export PATH="${PATH}:$(brew --prefix qt@5.5)/bin"
+export PATH="${PATH}:/usr/local/opt/qt@5.5/bin"
