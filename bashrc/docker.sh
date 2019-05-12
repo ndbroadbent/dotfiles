c1ae0cd401784c2630e309ebb5f92c36d15334c3
Nathan Broadbent <git@ndbroadbent.com>
2019-05-12 15:29:30 +0700

    Added docker_clean function

--- bashrc/docker.sh
+++ bashrc/docker.sh
@@ -3,3 +3,11 @@ alias dr="docker run --rm -it"
 dxb() { docker exec -it "$1" bash; }
 drb() { docker run --rm -it "$1" bash; }
 drc() { docker run --rm -it "$1" rails console; }
+
+# http://jimhoskins.com/2013/07/27/remove-untagged-docker-images.html
+docker_clean() {
+  echo "Removing all untagged Docker images..."
+  docker rmi $(docker images -a | grep "^<none>" | awk '{print $3}')
+  echo "Removing all stopped Docker containers..."
+  docker rm $(docker ps -a -q)
+}
