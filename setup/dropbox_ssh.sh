4f688cba9c1c0f10e2872469f94f10479651cebb
Nathan Broadbent <git@ndbroadbent.com>
2019-11-25 17:25:07 +0700

    Update SSH backup command

--- setup/dropbox_ssh.sh
+++ setup/dropbox_ssh.sh
@@ -2,10 +2,9 @@
 if [ "$(basename $(pwd))" = "setup" ]; then . _shared.sh; else . setup/_shared.sh; fi;
 echo "== Decrypting .ssh from Dropbox..."
 
-openssl aes-256-cbc -d -in $HOME/Dropbox/UbuntuSync/ssh.tar.gz.encrypted -out /tmp/ssh.tar.gz
+openssl aes-256-cbc -d -in "$HOME/Dropbox/ssh.tar.gz.encrypted" -out /tmp/ssh.tar.gz
 
-cd /tmp
-tar xvf ssh.tar.gz
-mkdir -p $HOME/.ssh
-cp -R /tmp/home/ndbroadbent/.ssh/* ~/.ssh/
-rm -rf /tmp/ssh.tar.gz /tmp/home/ndbroadbent/.ssh/ 
+mkdir -p "$HOME/.ssh"
+cd "$HOME/.ssh"
+tar xvf /tmp/ssh.tar.gz
+rm -rf /tmp/ssh.tar.gz
