ada99d9bfad313442328f0698ed5f61127b61dc1
Nathan Broadbent <git@ndbroadbent.com>
2019-11-30 01:10:02 +0700

    Updates

--- setup/packages.sh
+++ setup/packages.sh
@@ -2,6 +2,7 @@
 # Refs: https://gist.github.com/t-io/8255711
 
 if ! which brew > /dev/null 2>&1; then
+  echo "Installing homebrew..."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
@@ -11,13 +12,16 @@ fi
 
 brew install mackup rbenv nvm git bash wget curl yarn \
   ack vim nano less htop unrar ffmpeg \
-  postgresql imagemagick vim hugo watch heroku gnupg binutils diffutils ed \
+  postgres redis imagemagick vim hugo watch heroku gnupg binutils diffutils ed \
   gzip screen file-formula openssh python rsync unzip \
   findutils coreutils gawk gnu-indent gnu-sed gnu-tar gnu-which gnutls grep
 
 brew cask install flux google-chrome firefox iterm2 spectacle \
-  visual-studio-code virtualbox dropbox vlc charles skype spotify docker
+  visual-studio-code virtualbox dropbox vlc charles skype telegram spotify docker
 
-echo -e "\n====> Run 'mackup restore' after you've signed in to Dropbox.\n"
+echo -e "\n====> Run the following commands after you've signed in to Dropbox:"
+eho "      * mackup restore"
+eho "      * dropbox_backup ssh -r"
+eho "      * dropbox_backup convox -r"
 # echo "Running brew cleanup..."
 # brew cleanup
