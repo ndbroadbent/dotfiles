6770937fdcb864f94b167df8eb9c7d5d71a6be9a
Nathan Broadbent <git@ndbroadbent.com>
2019-12-01 01:33:27 +0700

    Open a bunch of applications, then show some next steps. Use mas to install apps from the App Store

---
+++ setup/mas.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Use mas to install apps from Mac App Store (https://github.com/mas-cli/mas)
+# See list of installed apps: $ mas list
+
+# read -p "What's your Apple ID? " APPLE_ID
+# mas signin --dialog "$APPLE_ID" || true
+# Error: The 'signin' command has been disabled on this macOS version. Please sign into the Mac App Store app manually.
+# For more info see: https://github.com/mas-cli/mas/issues/164
+
+# Apps:
+# ----------------------------------------------------------------
+MAC_APP_STORE_APPS=(
+  "Spark - Email App"
+  "LINE"
+  "Kindle"
+  "Evernote"
+  "Slack"
+)
+for APP_NAME in "${MAC_APP_STORE_APPS[@]}"; do
+  echo "+ mas search $APP_NAME"
+  mas search "$APP_NAME" | head -n1
+  echo "=> Installing $APP_NAME..."
+  mas lucky "$APP_NAME"
+done
