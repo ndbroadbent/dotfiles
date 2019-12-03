1952c7d28516f0d8e7afe0ae2255b0ccc36964a2
Nathan Broadbent <git@ndbroadbent.com>
2019-12-03 21:38:37 +0700

    Added db-browser-for-sqlite (https://sqlitebrowser.org/)

--- setup/packages.sh
+++ setup/packages.sh
@@ -34,7 +34,7 @@ mkdir -p "$HOME/.nvm"
 brew cask install google-chrome firefox \
   flux rectangle dozer rescuetime \
   dropbox google-backup-and-sync \
-  iterm2 visual-studio-code docker virtualbox postico charles \
+  iterm2 visual-studio-code docker virtualbox postico db-browser-for-sqlite charles \
   skype telegram spotify vlc sketch
 
 # echo "Running brew cleanup..."
