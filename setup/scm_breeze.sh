14dca5fb2301a013e0bd7f2cbaf21412c41c8659
Nathan Broadbent <ndbroadbent@Nathans-MacBook-Pro.local>
2019-12-04 13:25:46 +0800

    Fix scm_breeze install script

--- setup/scm_breeze.sh
+++ setup/scm_breeze.sh
@@ -4,7 +4,8 @@ if [ -d "$HOME/code/scm_breeze" ]; then
 else
   mkdir -p "$HOME/code"
   (
-    git clone git://github.com/ndbroadbent/scm_breeze.git
+    cd "$HOME/code"
+    git clone git://github.com/ndbroadbent/scm_breeze.git scm_breeze
     cd scm_breeze
     source install.sh
   )
