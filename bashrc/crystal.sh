486b250d2cdecffd0913ed16af1ae64fa059f15c
Nathan Broadbent <git@ndbroadbent.com>
2019-12-08 15:04:21 +0700

    Need export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig for Crystal

--- bashrc/crystal.sh
+++ bashrc/crystal.sh
@@ -1,2 +1,3 @@
 # Use ./bin/crystal during development of the Crystal language
+export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
 alias crystal="CRYSTAL_BIN=\$([ -f ./bin/crystal ] && echo ./bin/crystal || echo \$(which crystal)); \$CRYSTAL_BIN"
