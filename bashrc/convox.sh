f8e9c2189d566ed928bc743341470c677369273b
Nathan Broadbent <git@ndbroadbent.com>
2020-09-21 00:06:48 +0800

    Added convox2 and convox3 aliases to switch between versions

--- bashrc/convox.sh
+++ bashrc/convox.sh
@@ -27,3 +27,12 @@ alias cxi="convox instances"
 alias cxrl="convox releases"
 alias cxb="convox builds"
 alias cxps="convox ps"
+
+# Easy way to switch between versions.
+# Install v3: https://docs.convox.com/installation/cli
+# Install v2: https://github.com/convox/docs/blob/8b3dfeef207711d442295afdb08c88491b0869a4/docs/introduction/installation.md
+# See also: https://community.convox.com/t/what-happened-to-gen-2-cli-downloads/741
+# https://docsv2.convox.com/introduction/installation
+# Upgrading: https://docs.convox.com/help/upgrading
+alias convox2="set -x && sudo ln -fs /usr/local/bin/convox2 /usr/local/bin/convox && set +x"
+alias convox3="set -x && sudo ln -fs /usr/local/bin/convox3 /usr/local/bin/convox && set +x"
