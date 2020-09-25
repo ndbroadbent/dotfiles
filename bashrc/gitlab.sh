74733ae449758797c31e1950ab600f201d998c60
Nathan Broadbent <git@ndbroadbent.com>
2020-09-26 00:29:00 +0800

    Added -o merge_request.create option

--- bashrc/gitlab.sh
+++ bashrc/gitlab.sh
@@ -1,5 +1,5 @@
 # See: https://docs.gitlab.com/ee/user/project/push_options.html
 
-# Configure any merge requests to get automatically merged when
-# the CI build is successful
-alias gpsm="git push -o merge_request.merge_when_pipeline_succeeds"
+# Create a new merge request if it doesn't already exist.
+# Configure any merge requests to get automatically merged when the CI build is successful.
+alias gpsm="git push -o merge_request.create -o merge_request.merge_when_pipeline_succeeds"
