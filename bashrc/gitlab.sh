bab9582f96559f2f1cb704e4e4d92c19d703f1ae
Nathan Broadbent <git@ndbroadbent.com>
2022-06-10 18:02:38 +1200

    gpsm should wait for CI build to finish and notify me

--- bashrc/gitlab.sh
+++ bashrc/gitlab.sh
@@ -3,4 +3,4 @@
 # Create a new merge request if it doesn't already exist.
 # Update: don't automatically merge when pipeline succeeds (-o merge_request.merge_when_pipeline_succeeds)
 # We deploy from branches, and then merge to main after it's deployed.
-alias gpsm="git push -o merge_request.create -o merge_request.target=main && git checkout main"
+alias gpsm="git push -o merge_request.create -o merge_request.target=main && [ -f scripts/wait_for_ci_build ] && scripts/wait_for_ci_build"
