02a88723503bc4ed240be59cd6d94dd5e39aa4fa
Nathan Broadbent <git@ndbroadbent.com>
2022-06-17 21:25:08 +1200

    Added gmm and gmmp aliases

--- bashrc/gitlab.sh
+++ bashrc/gitlab.sh
@@ -4,3 +4,7 @@
 # Update: don't automatically merge when pipeline succeeds (-o merge_request.merge_when_pipeline_succeeds)
 # We deploy from branches, and then merge to main after it's deployed.
 alias gpsm="git push -o merge_request.create -o merge_request.target=main && [ -f scripts/wait_for_ci_build ] && scripts/wait_for_ci_build"
+
+alias gmm="export CURRENT_BRANCH=\"\$(git rev-parse --abbrev-ref HEAD 2> /dev/null)\" \
+&& git checkout main && git pull origin main && git merge \$CURRENT_BRANCH"
+alias gmmp="gmm && git push origin main && (git branch -D \$CURRENT_BRANCH || true) && git push origin :\$CURRENT_BRANCH"
