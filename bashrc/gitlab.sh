0af53cd39bff3620b805a0a98310b1beeb3dc982
Nathan Broadbent <git@ndbroadbent.com>
2022-06-22 12:34:11 +1200

    Move into docspring repo

--- bashrc/gitlab.sh
+++ bashrc/gitlab.sh
@@ -8,32 +8,3 @@ alias gpsm="git push -o merge_request.create -o merge_request.target=main && [ -
 alias gmm="export CURRENT_BRANCH=\"\$(git rev-parse --abbrev-ref HEAD 2> /dev/null)\" \
 && git checkout main && git pull origin main && git merge \$CURRENT_BRANCH"
 alias gmmp="gmm && git push origin main && (git branch -D \$CURRENT_BRANCH || true) && git push origin :\$CURRENT_BRANCH"
-
-push_wait_deploy() {
-  export CURRENT_GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-  if [ "$?" != '0' ] || [ -z "$CURRENT_GIT_BRANCH" ]; then
-    echo "Error fetching current git branch"
-    return 1
-  fi
-
-  git push -o merge_request.create -o merge_request.target=main || return
-  ./scripts/wait_for_ci_build || return
-
-  ( osascript -e "display notification \"Get ready to touch your Yubikey for 2FA...\" with title \"Deploying...\"" &) &> /dev/null
-  ./scripts/deploy || return
-
-  LATEST_GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD 2> /dev/null)"
-  # Check if branch is the same
-  if [ "$CURRENT_GIT_BRANCH" != "$LATEST_GIT_BRANCH" ]; then
-    echo "Git branch has changed. Please run 'gmmp' to merge your changes into main."
-    return 1
-  fi
-
-  git checkout main || return
-  git pull origin main || return
-  git merge "$CURRENT_GIT_BRANCH" || return
-
-  git push origin main || return
-  (git branch -D $CURRENT_GIT_BRANCH || true)
-  git push origin ":$CURRENT_GIT_BRANCH" || return
-}
