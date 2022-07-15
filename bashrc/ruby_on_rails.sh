23b72d0490f1e284e1a7706abbc95df501767170
Nathan Broadbent <git@ndbroadbent.com>
2022-07-15 17:18:59 +1200

    Set THOR_MERGE variable for rails upgrades

--- bashrc/ruby_on_rails.sh
+++ bashrc/ruby_on_rails.sh
@@ -3,3 +3,5 @@ rgm()     { rails_cmd generate migration "$@" --editor; }
 
 alias rubomaster="be rubocop \$(git diff HEAD..origin/master --name-only | grep '\.rb')"
 alias rubomain="be rubocop \$(git diff HEAD..origin/main --name-only | grep '\.rb')"
+
+export THOR_MERGE="code -d \$1 \$2"
