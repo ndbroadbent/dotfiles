281381ee0651598e994258bfce6a3c80666d1e9b
Nathan Broadbent <git@ndbroadbent.com>
2022-11-23 15:36:01 +1300

    gpsp alias: open PR and pipeline in web browser

--- bashrc/github.sh
+++ bashrc/github.sh
@@ -5,6 +5,15 @@
 gpsp() (
   set -euo pipefail
   git push
-  gh pr create --fill
+  gh pr create --fill || true
+  gh pr view --web
+
+  local CURRENT_GIT_BRANCH CURRENT_GIT_COMMIT
+  CURRENT_GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+  CURRENT_GIT_COMMIT="$(git rev-parse HEAD)"
+  PIPELINE_URL="$(./scripts/circleci_pipeline_status -f "%u" -b "$CURRENT_GIT_BRANCH" -c "$CURRENT_GIT_COMMIT")"
+  local CHROME_BIN="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+  "$CHROME_BIN" "${PIPELINE_URL}" 2>/dev/null
+
   [ -f scripts/wait_for_ci_build ] && scripts/wait_for_ci_build
 )
