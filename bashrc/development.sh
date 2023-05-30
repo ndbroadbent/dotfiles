fde9765b9947438552897637f716cc1e3f1b45b8
Nathan Broadbent <git@ndbroadbent.com>
2023-05-30 18:28:30 +1200

    Updates to dev function

--- bashrc/development.sh
+++ bashrc/development.sh
@@ -47,17 +47,29 @@ alias stb="short st --git-branch-short"
 # - Start git branch for current story in development
 # - Open story in new Chrome window
 dev() (
+  local CURRENT_GIT_BRANCH STORY_ID STORY_COUNT STORY_JSON STORY_URL SENTRY_URL
+
   set -euo pipefail
 
   cd ~/code/docspring
+
+  if ! \git diff --quiet 2>/dev/null; then
+    echo "=> Git working tree is dirty. Please commit or stash your changes and try again." >&2
+    return 1
+  fi
+  CURRENT_GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+  if [[ "$CURRENT_GIT_BRANCH" != "main" ]]; then
+    echo "=> Checking out main branch..." >&2
+    \git checkout main
+  fi
+
   if [[ "${_DEV_SCRIPT:-}" != "true" ]]; then
+    echo "=> Opening iTerm2 tabs..." >&2
     osascript .dev.scpt "$(pwd)"
   else
     osascript -e 'tell application "iTerm" to activate' \
               -e 'tell application "System Events" to keystroke "f" using {command down, option down}'
   fi
-
-  local STORY_ID STORY_COUNT STORY_JSON STORY_URL SENTRY_URL
 
   SHORT_SEARCH='owner:nathan state:"in development"'
   echo "Searching for stories: $SHORT_SEARCH" >&2
@@ -67,10 +79,11 @@ dev() (
   if [ "$STORY_COUNT" -eq 0 ]; then
     echo "No stories found." >&2
     return 1
-  elif [ "$STORY_COUNT" -gt 1 ]; then
-    echo "Multiple stories found with state 'In Development':" "${STORY_IDS[*]}" >&2
-    return 1
+  # elif [ "$STORY_COUNT" -gt 1 ]; then
+  #   echo "Multiple stories found with state 'In Development':" "${STORY_IDS[*]}" >&2
+  #   return 1
   fi
+  # Use the first story in the list
   STORY_ID="${STORY_IDS[0]}"
 
   echo "Fetching story JSON..." >&2
@@ -82,13 +95,14 @@ dev() (
 
   echo "=> Opening story in new Google Chrome window: ${STORY_URL}" >&2
   CHROME_BIN="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-  "$CHROME_BIN" --new-window "${STORY_URL}" 2>/dev/null
+  "$CHROME_BIN" --new-window "${STORY_URL}" 2>/dev/null &
+  sleep 3
 
   OPENED_SENTRY_URL='false'
   # Open any external links
   for EXTERNAL_LINK in $(echo "$STORY_JSON" | jq -r '.external_links | join("\n")'); do
     echo "=> Opening external link: ${EXTERNAL_LINK}" >&2
-    "$CHROME_BIN" "${EXTERNAL_LINK}" 2>/dev/null
+    "$CHROME_BIN" "${EXTERNAL_LINK}" 2>/dev/null &
 
     if [[ "${EXTERNAL_LINK}" == *"sentry.io"* ]]; then
       OPENED_SENTRY_URL='true'
@@ -102,15 +116,9 @@ dev() (
     # Open Sentry error (if present)
     if [ -n "$SENTRY_URL" ]; then
       echo "=> Opening Sentry error: ${SENTRY_URL}" >&2
-      "$CHROME_BIN" "${SENTRY_URL}" 2>/dev/null
+      "$CHROME_BIN" "${SENTRY_URL}" 2>/dev/null &
     fi
   fi
-
-  # Open DocSpring development URLs
-  for DEV_URL in "http://admin.docspring.local:3000" "http://app.docspring.local:3000"; do
-    echo "=> Opening development URL: ${DEV_URL}" >&2
-    "$CHROME_BIN" "$DEV_URL" 2>/dev/null
-  done
 
   local WAS_RUNNING="false"
   if pgrep -f 'Visual Studio Code' > /dev/null; then
@@ -129,4 +137,36 @@ dev() (
 
   # Close all editor tabs and collapse all files in VS Code
   osascript "${DOTFILES_PATH}/applescript/reset-vscode.scpt"
+
+  sleep 1
+  osascript -e 'tell application "iTerm" to activate'
+
+  echo "=> Starting Rails server..." >&2
+  bin/rails server --binding=127.0.0.1 --port 3000 &
+
+  echo "=> Waiting for Rails server to be ready..." >&2
+  while ! curl -s http://app.docspring.local:3000/health > /dev/null; do
+    sleep 1
+    printf '.'
+  done
+  echo
+  echo "=> Rails server is ready!" >&2
+  sleep 1
+
+  # Open DocSpring development URLs
+  for DEV_URL in "http://admin.docspring.local:3000" "http://app.docspring.local:3000"; do
+    echo "=> Opening development URL: ${DEV_URL}" >&2
+    "$CHROME_BIN" "$DEV_URL" 2>/dev/null &
+    sleep 1
+  done
+
+  # osascript -e 'tell application "Visual Studio Code" to activate'
+  osascript -e 'tell application "Google Chrome" to activate'
+  osascript -e 'tell application "Google Chrome" to set active tab index of last window to 1'
+
+  osascript -e "display notification \"Story ${STORY_ID} is ready for development!\" with title \"Dev Setup Finished!\""
+
+  wait
+
+  osascript .dev.railsserver.scpt "$(pwd)"
 )
