7eb8523533ade3fa8b9ecd4b32bf3de1a31b18f0
Nathan Broadbent <git@ndbroadbent.com>
2023-08-18 16:05:12 +1200

    more fish aliases

--- fish/config.fish
+++ fish/config.fish
@@ -61,4 +61,30 @@ abbr -a grsl 'git reset HEAD~'
 abbr -a gcm 'git commit --amend'
 abbr -a gcmh 'git commit --amend -C HEAD'
 
+function grbi
+    if test (count $argv) -eq 0
+        git rebase -i HEAD~10
+    else
+        git rebase -i $argv
+    end
+end
+
+alias gpsp 'bash -c '"'"'
+git_push_open_pr_wait_for_ci() {
+  set -euo pipefail
+  echo "Pushing changes to GitHub..."
+  git push
+  gh pr create --fill || true
+  gh pr view --web
+  local CURRENT_GIT_BRANCH CURRENT_GIT_COMMIT
+  CURRENT_GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+  CURRENT_GIT_COMMIT="$(git rev-parse HEAD)"
+  PIPELINE_URL="$(./scripts/circleci_pipeline_status -f "%u" -b "$CURRENT_GIT_BRANCH" -c "$CURRENT_GIT_COMMIT")"
+  local CHROME_BIN="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+  "$CHROME_BIN" "${PIPELINE_URL}" 2>/dev/null
+  [ -f scripts/wait_for_ci_build ] && scripts/wait_for_ci_build
+}
+git_push_open_pr_wait_for_ci
+'"'"
+
 abbr -a tmk "tmux kill-session"
