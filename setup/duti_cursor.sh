3cf9e9db7fcf55a06a057c29d01e5699a14c87c8
Nathan Broadbent <git@ndbroadbent.com>
2024-09-24 14:15:32 +1200

    open files with Cursor

---
+++ setup/duti_cursor.sh
@@ -0,0 +1,82 @@
+#? /bin/bash
+#
+# Change default editor for the listed filetypes on MacOS.
+# Required duti to be installed. `brew install duti`
+
+extensions=(
+  public.plain-text
+  public.unix-executable
+  .c
+  .cpp
+  .cs
+  .css
+  .env
+  .erb
+  .go
+  .haml
+  .hbs
+  .hs
+  .java
+  .js
+  .json
+  .lock
+  .map
+  .md
+  .php
+  .rb
+  .rake
+  Gemfile
+  Rakefile
+  .lock
+  .sass
+  .scss
+  .sh
+  .fish
+  .svg
+  .ts
+  .txt
+  .xml
+  .yaml
+  .yml
+  .zsh
+)
+vscodeinsiders=com.microsoft.VSCodeInsiders
+vscode=com.microsoft.VSCode
+atom=com.github.atom
+sublime2=com.sublimetext.2
+sublime3=com.sublimetext.3
+macvim=org.vim.MacVim
+cursor=com.todesktop.230313mzl4w4u92
+
+editor=$cursor
+
+# editors=("Atom" "MacVim" "SublimeText2" "SublimeText3" "VisualStudioCode" "VisualStudioCodeInsiders")
+
+# echo "Which editor would you like to be default?"
+
+# select opt in ${editors[@]}; do
+#   if [ "$opt" = "Atom" ]; then
+#     editor=$atom
+#     break
+#   elif [ "$opt" = "MacVim" ]; then
+#     editor=$macvim
+#     break
+#   elif [ "$opt" = "SublimeText2" ]; then
+#     editor=$sublime2
+#     break
+#   elif [ "$opt" = "SublimeText3" ]; then
+#     editor=$sublime3
+#     break
+#   elif [ "$opt" = "VisualStudioCode" ]; then
+#     editor=$vscode
+#     break
+#   elif [ "$opt" = "VisualStudioCodeInsiders" ]; then
+#     editor=$vscodeinsiders
+#     break
+#   fi
+# done
+
+for extension in "${extensions[@]}"; do
+  echo "duti -s $editor $extension"
+  duti -s $editor "$extension" all
+done
