93624e3eb2b831ba93a98e79cfe5e0dfeb279b47
Nathan Broadbent <git@ndbroadbent.com>
2025-02-28 10:24:53 +1300

    aliases

--- bashrc/aliases.sh
+++ bashrc/aliases.sh
@@ -44,6 +44,10 @@ alias python="python3"
 alias py="python3"
 alias pip="pip3"
 
+alias gfp="git fetch --prune"
+# cspell:disable-next-line
+alias gmain="git checkout main && git pull --prune && git-clean-branches"
+
 # This is a stupid idea. if you want bat, type bat.
 # alias cat='bat'
 
